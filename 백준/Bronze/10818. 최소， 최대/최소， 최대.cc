a6c75cb80ada427a786f83208f5232099f52cae2
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2022-09-02 15:26:29 +0900

    [Bronze III] Title: 최소, 최대, Time: 460 ms, Memory: 5804 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{	//첫째 줄에 정수의 개수 N을 입력
+	int N;
+	cin >> N;
+
+	int max = -1000001;
+	int min = 1000001;//-1000000<=N<=1000000
+	//둘째 줄은 for문으로 돌려서 배열 입력
+	int arr[N];
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i];
+		if (arr[i] > max) max = arr[i];//arr의 최댓값을 max로 저장
+		if (arr[i] < min) min = arr[i];//arr의 최솟값을 min으로 저장
+		
+	}
+		cout << min << " " << max;
+	return 0;
+}
