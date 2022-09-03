94a7f1b6dddf6dc85e15fe455111c7719f1d07f7
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2022-09-03 19:39:09 +0900

    오버플로우 에러를 new,delete연산자를 통한 동적메모리할당으로 해결
    배열의 기본값 크기 = 1MB = 1,000,000byte인데, 문제의 조건이 1<=N<=1,000,000 즉, 1,000,000+a(공백)이라 오버플로우 에러 발생
    ->동적 메모리 할당으로 에러 해결!!

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -1,22 +1,27 @@
 #include <iostream>
 
 using namespace std;
-int main()
-{	//첫째 줄에 정수의 개수 N을 입력
-	int N;
-	cin >> N;
 
-	int max = -1000001;
-	int min = 1000001;//-1000000<=N<=1000000
-	//둘째 줄은 for문으로 돌려서 배열 입력
-	int arr[1000000]; //배열 크기 선언
-	for (int i = 0; i < N; i++)
+int main()
+{
+	//첫째 줄에 정수의 개수 N
+	//-1,000,000<=N<=1,000,000:최댓값, 최솟값 설정
+	int n;
+	cin >> n;
+
+	int max = -1000000;
+	int min = 1000000;
+	int* arr = new int[n]; //int형 포인터 변수를 배열처럼 취급, int형 메모리를 찾아줘
+	for (int i = 0; i < n; i++)
 	{
-		cin >> arr[i];
-		if (arr[i] > max) max = arr[i];//arr의 최댓값을 max로 저장
-		if (arr[i] < min) min = arr[i];//arr의 최솟값을 min으로 저장
-		
+		cin >> *arr; //arr의 값을 n개만큼 입력
+
+		if (*arr > max) max = *arr;
+		if (*arr < min) min = *arr;
 	}
-		cout << min << " " << max;
+
+	cout << min << " " << max;
+
+	delete[] arr; // 사용한 메모리 환수
 	return 0;
 }
