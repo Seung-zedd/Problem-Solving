bd753a375d9925f13527c067def809e6ab3794bd
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2023-02-02 02:28:18 +0900

    [Bronze II] Title: 나머지, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,11 +1,9 @@
 import sys
 input = sys.stdin.readline
-lst = []
+lst = set()
 
 for i in range(10):
-    lst.append(int(input()) % 42) 
-
-lst = set(lst)
+    lst.add(int(input()) % 42) # update:요소 여러개 추가, 만약 update함수를 쓰고 싶다면 [] 또는 {}로 1개 이상의 인자값을 넣어야함
 print(len(lst))
     
     
