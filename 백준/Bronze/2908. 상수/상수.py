8c672a2a95de45516a5a134ca9ca4073595e9e9c
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2023-02-02 11:18:20 +0900

    [Bronze II] Title: 상수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -2,10 +2,7 @@ import sys
 input = sys.stdin.readline
 
 a, b = input().split()
-a = list(a) # ['7', '3', '4']
-b = list(b)
-new_a = a[-1]+a[-2]+a[-3]
-new_b = b[-1]+b[-2]+b[-3]
-print(max(new_a, new_b))
-
+new_a = a[::-1]
+new_b = b[::-1]
+print(max(new_a,new_b))
     
