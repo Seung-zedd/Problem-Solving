9c5f5ad46fdfca81b7de24d159bbedd7ba5e8e6d
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2023-02-02 11:23:56 +0900

    [Bronze II] Title: 상수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -2,7 +2,10 @@ import sys
 input = sys.stdin.readline
 
 a, b = input().split()
-new_a = a[::-1]
-new_b = b[::-1]
+a = list(a)
+b = list(b)
+a.reverse(), b.reverse() # 리스트를 역순으로
+new_a = ''.join(a) # join:역순의 리스트를 문자열로 합침
+new_b = ''.join(b)
 print(max(new_a,new_b))
     
