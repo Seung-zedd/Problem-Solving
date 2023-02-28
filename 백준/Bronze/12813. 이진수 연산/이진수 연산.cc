1785a609b871d4f3b749851fd851d804145b6261
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2023-02-28 16:13:28 +0900

    [Bronze II] Title: 이진수 연산, Time: 16 ms, Memory: 2500 KB -BaekjoonHub

--- 백준/Bronze/12813. 이진수 연산/이진수 연산.cc
+++ 백준/Bronze/12813. 이진수 연산/이진수 연산.cc
@@ -1,45 +1,44 @@
 #include <iostream>
-#include <cstring>
+
 using namespace std;
 
 int main()
 {
 	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	// C와 C++사이의 stream 동기화를 끊음
-	char A[100001];
-	char B[100001];
+	string A, B;
 
 	cin >> A >> B;
 
-	for (int i = 0; i < strlen(A); i++) // AND
+	for (int i = 0; i < A.size(); i++) // AND
 	{
 		if (A[i] == '1' && B[i] == '1') cout << '1';
 		else cout << '0';
 	}
 	cout << endl;
 
-	for (int i = 0; i < strlen(A); i++) // OR
+	for (int i = 0; i < A.size(); i++) // OR
 	{
 		if (A[i] == '1' || B[i] == '1') cout << '1';
 		else cout << '0';
 	}
 	cout << endl;
 
-	for (int i = 0; i < strlen(A); i++) // XOR(odd function)
+	for (int i = 0; i < A.size(); i++) // XOR(odd function)
 	{
 		if (A[i] !=B[i]) cout << '1';
 		else cout << '0';
 	}
 	cout << endl;
 
-	for (int i = 0; i < strlen(A); i++) // NOT A
+	for (int i = 0; i < A.size(); i++) // NOT A
 	{
 		if (A[i] == '1') cout << '0';
 		else cout << '1';
 	}
 	cout << endl;
 
-	for (int i = 0; i < strlen(A); i++) // NOT B
+	for (int i = 0; i < A.size(); i++) // NOT B
 	{
 		if (B[i] == '1') cout << '0';
 		else cout << '1';
