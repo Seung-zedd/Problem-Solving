cc55dd2d7efaef9e6ad0c0fc6f3b7be50495694d
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2023-09-09 10:32:04 +0900

    [Silver IV] Title: 카드2, Time: 168 ms, Memory: 128136 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,16 +1,17 @@
-
 from collections import deque
 
+import sys  
+input = sys.stdin.readline
+
+N = int(input())
 d = deque()
-ls = []
-N = int(input()) # 테스트 케이스 개수
-# N장의 카드만큼 카드를 추가
+
+# 1~N번의 카드 생성
 for x in range(1, N+1):
     d.append(x)
 
+# 1), 2) 과정 반복
 for _ in range(N-1):
-    d.popleft() # 위의 카드를 버림
-    ls.append(d[0]) # 프론트를 빈 리스트에 저장
-    d.popleft() # 복사당한 원소를 제거
-    d.append(ls[-1]) # 저장한 원소를 덱에 추가
+    d.popleft()
+    d.append(d.popleft())
 print(*d)
