dc79185944ee97c430f85a4225df22fa62eb2d92
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2023-09-13 17:11:05 +0900

    [Silver IV] Title: 괄호, Time: 120 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,32 +1,24 @@
 import sys
 input = sys.stdin.readline
 
-'''
-만일 입력 괄호 문자열이 올바른 괄호 문자열(VPS)이면 “YES”, 아니면 “NO”를 한 줄에 하나씩 차례대로 출력해야 한다. 
-'''
-
 N = int(input()) # 테스트 케이스 개수
-stack = []
 
 def isValid(s):
+    stack = []
     for x in s:
         if x == '(':
             stack.append(')')
-        else:
-            while not stack or stack.pop() != x:
+        
+        elif not stack or stack.pop() != x:
                 print("NO")
-                stack.clear()
                 return False
     if stack:
         print("NO")
-        stack.clear()
         return False
     else:
         print("YES")
-        stack.clear()
         return True
-    
+
 for _ in range(N):
     s = input().rstrip('\n')
     isValid(s)
-    
