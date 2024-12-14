c4668b852620e57bb531e82c282c4d36209dbf7b
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2024-12-14 12:13:36 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -2,13 +2,8 @@ def solution(array):
     dict = {}
 
     for num in array:
-        # Key가 없으면 0을 리턴
-        # l-value는 value로 저장하기 위한 것이고, r-value는 리터럴 값임
         dict[num] = dict.get(num, 0) + 1 # r-value is 1 or num + 1
     max_count = max(dict.values())
     result = [k for k, v in dict.items() if v == max_count]
 
-    if len(result) >= 2:
-        return -1
-    else:
-        return result[0]
+    return -1 if len(result) >= 2 else result[0]
