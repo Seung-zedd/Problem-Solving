710fe723856cf41c7a22b48fbc71b6cc10614eb2
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2024-12-14 11:52:42 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,18 +1,14 @@
 def solution(array):
-    memo = {}
+    dict = {}
 
-    # array에 있는 원소들을 순회하면서 카운팅을 함
-    # num이 없으면 애초에 카운팅이 x
     for num in array:
-        memo[num] = memo.get(num, 0) + 1
+        # Key가 없으면 0을 리턴
+        # l-value는 value로 저장하기 위한 것이고, r-value는 리터럴 값임
+        dict[num] = dict.get(num, 0) + 1 # r-value is 1 or num + 1
+    max_count = max(dict.values())
+    result = [k for k, v in dict.items() if v == max_count]
 
-    # 최빈값의 빈도 계산
-    max_count = max(memo.values())
-    result = [key for key, value in memo.items() if value == max_count]
-
-    # 최빈값이 2개 이상이면 -1을 리턴
     if len(result) >= 2:
         return -1
-    # 그렇지 않으면 mode를 리턴
     else:
         return result[0]
