22b03140136325a95453a1b0e0acedeb924d6176
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2024-12-15 19:48:12 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/README.md
+++ 프로그래머스/0/120812. 최빈값 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 최빈값 구하기 - 120812 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120812) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120812?language=python3) 
 
 ### 성능 요약
 
-메모리: 4.13 MB, 시간: 0.02 ms
+메모리: 10.1 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 15일 19:26:39
+2024년 12월 15일 19:48:07
 
 ### 문제 설명
 
--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,9 +1,21 @@
 def solution(array):
-    dict = {}
+    memo = {}
 
+    # array에 있는 원소들을 순회하면서 카운팅을 함
+    # num이 없으면 애초에 카운팅이 x
     for num in array:
-        dict[num] = dict.get(num, 0) + 1 # r-value is 1 or num + 1
-    max_count = max(dict.values())
-    result = [k for k, v in dict.items() if v == max_count]
+        memo[num] = memo.get(num, 0) + 1
 
-    return -1 if len(result) >= 2 else result[0]
+    # 최빈값의 빈도 계산
+    # items를 순회하는데, value가 내가 찾는 max_count와 일치하면 key를 result 리스트에 append
+    max_count = max(memo.values())
+
+    result_count = 0
+    mode = -1
+
+    for k, v in memo.items():
+        if v == max_count:
+            result_count += 1
+            mode = k
+    # corrected using ternary operator
+    return -1 if result_count > 1 else mode
