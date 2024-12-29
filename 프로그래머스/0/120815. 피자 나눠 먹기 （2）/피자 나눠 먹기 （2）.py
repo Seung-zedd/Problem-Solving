df4fcf26d8b613f471947997842054fdc33ca155
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2024-12-29 14:12:37 +0900

    Update 피자 나눠 먹기 （2）.py

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -1,14 +1,7 @@
 def solution(n):
     def gcd(p, q):
-        # 1 <= n <= 6
-        if p <= 6:
-            q, p = p, q
-            if p == 0:
-                return q
-            return gcd(p, q % p)
-        else:
-            # base condition
-            if q == 0:
-                return p
-            return gcd(q, p % q)
+        # modulo 연산 특징 때문에 굳이 case 분류해서 swap해주지 않아도됨
+        if q == 0:
+            return p
+        return gcd(q, p % q)
     return n // gcd(n, 6)
