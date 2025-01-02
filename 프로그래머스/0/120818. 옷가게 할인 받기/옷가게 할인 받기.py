751b2afb596f14ec216115233b9a611c9047a97a
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-01-02 14:39:32 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,9 +1,9 @@
 def solution(price):
-    if price < 100000:
-        return price 
-    elif 100000 <= price < 300000:
-        return int(price * 0.95)
-    elif 300000 <= price < 500000:
-        return int(price * 0.9)
-    else:
+    if price >= 500000:
         return int(price * 0.8)
+    elif price >= 300000:
+        return int(price * 0.9)
+    elif price >= 100000:
+        return int(price * 0.95)
+    else:
+        return price
