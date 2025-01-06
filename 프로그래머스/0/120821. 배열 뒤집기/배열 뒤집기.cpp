e6d89a71c3ec4620f385ca7e6bd2b3d844f414ce
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-01-06 12:16:48 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cpp
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 vector<int> solution(vector<int> num_list)
 {
-    reverse(begin(num_list), end(num_list));
+    // reverse(begin(num_list), end(num_list));
+    reverse(num_list.begin(), num_list.end());
     return num_list;
 }
