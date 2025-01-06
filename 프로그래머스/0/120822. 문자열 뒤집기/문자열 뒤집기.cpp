00fff35fb8ed5761560f5ec9f9569e1a92ba7314
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-01-06 12:37:04 +0900

    Update 문자열 뒤집기.cpp

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.cpp
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.cpp
@@ -1,4 +1,7 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <string>
+#include <algorithm>
+// #include <bits/stdc++.h> <- 비표준 라이브러리이므로, 다른 컴파일러와 충돌할 우려가 있으므로 사용하지 말 것!
 
 using namespace std;
 
