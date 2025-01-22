2f4f69539b5b7dccc2145edbefa9daa90d7c3495
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-01-22 17:17:40 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,12 +1,6 @@
 def solution(num_list, n):
-
-    # 리스트 컴프리헨션을 사용해서 2차원 배열 생성
     mul = len(num_list) // n
-    arr = [[False for col in range(n)] for row in range(mul)]
-
-    # 만들어진 arr 1차원 배열에 num_list를 슬라이싱한 ls를 각각 얕은 복사
-    for i in range(mul):
-        # [0:3] ~ [3:6] ~[6:9] ~ ...
-        ls = num_list[i * n : (i + 1) * n]
-        arr[i] = ls
-    return arr
+    answer = []
+    
+    answer = [num_list[i * n : (i + 1) * n] for i in range(mul)]
+    return answer
