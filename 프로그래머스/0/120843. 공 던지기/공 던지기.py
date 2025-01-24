3939ee721ae062a3b5887f0437fa386b1d113304
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-01-24 16:06:27 +0900

    [level 0] Title: 공 던지기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.py
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.py
@@ -1,9 +1,2 @@
 def solution(numbers, k):
-    # numbers에 있는 원소들을 앞에는 홀수, 뒤에는 짝수 원소로 재구성
-    odd_numbers = [x for x in numbers if x % 2 != 0]
-    rearrange_numbers = [x for x in numbers if x % 2 != 0] + [x for x in numbers if x % 2 == 0]
-
-    if len(numbers) % 2 == 0:
-        return odd_numbers[(k - 1) % (len(numbers) // 2)]
-    else:
-        return rearrange_numbers[(k - 1) % len(rearrange_numbers)]
+    return numbers[2 * (k - 1) % len(numbers)]
