4fa1ed1f800dd0bc2b1f7415803e46a8fc85421f
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-01-29 01:19:16 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,3 +1,2 @@
 def solution(num_list, n):
-    answer = []
-    return [num_list[i : i + n] for i in range(0, len(num_list), n)]
+    return [num_list[x : x + n] for x in range(0, len(num_list), n)]
