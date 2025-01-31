e4f47549d15cb8deedf352b2a4c5cd3c7168d7b0
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-01-31 11:48:55 +0900

    [level 0] Title: 합성수 찾기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,10 +1,11 @@
 def solution(n):
-    not_composite = [1]
-    is_prime = {} # {num: count}
-    for x in range(1, n + 1):
-        for y in range(2, x + 1):
-            if x % y == 0:
-                is_prime[x] = is_prime.get(x, 0) + 1
-        if is_prime.get(x) == 1:
-            not_composite.append(x)
-    return n - len(not_composite)
+    is_prime = [True] * (n + 1)
+    is_prime[1] = False # 1은 제외
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if not is_prime[i]: continue
+        for j in range(2 * i, n + 1, i):
+            is_prime[j] = False
+    prime_ls = [x for x in is_prime[2:] if x]
+
+    return (n - 1) - len(prime_ls)
