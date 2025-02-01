838f43bd5212e7fada564479ff2441297ebcaa3d
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-02-01 22:11:35 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
@@ -1,12 +1,3 @@
 def solution(numbers):
-    ls = []
-
     numbers.sort()
-    max_val = max(numbers)
-    ls.append(max_val)
-    numbers.pop()
-
-    max_val_2 = max(numbers)
-    ls.append(max_val_2)
-
-    return ls[0] * ls[1]
+    return numbers[-2] * numbers[-1] # Using reverse indexing
