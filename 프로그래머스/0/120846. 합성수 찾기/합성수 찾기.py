65c98ede84ddc874b287bc37259d87812e632814
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-02-01 21:56:26 +0900

    Update 합성수 찾기.py

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,6 +1,6 @@
 def solution(n):
-    # (n-1) - len(소수)인데, 1은 제외됬고, zero-based idx이므로 서로 상쇄되므로, 그냥 n - len(is_prime)
-    is_prime = [True] * (n + 1)
+    # (n-1) - len(소수)인데, 1은 제외됬고, zero-based idx라 서로 상쇄되므로, 그냥 n - len(is_prime)
+    is_prime = [True] * (n + 1) # 0 <= x <= N == (N+1) 개수
     is_prime[1] = False
 
     for i in range(2, int(n ** 0.5) + 1): # 1차 필터링
