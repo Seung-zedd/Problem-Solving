53e89dc25d8761b87b2f7d2f8c4fec8f04b2af21
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-02-19 09:10:39 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,18 +1,2 @@
 def solution(my_string):
-    ls = my_string.split()
-    op = "" # to store state
-    res = [] # to store temporary result
-    
-    for s in ls:
-        if s == '+' or s == '-':
-            op = s
-        else: res.append(int(s))
-        if len(res) == 2 and op == '+':
-            temp = sum(res)
-            res = []
-            res.append(temp)
-        elif len(res) == 2 and op == '-':
-            temp = res[0] - res[1]
-            res = []
-            res.append(temp)
-    return res[0]
+    return sum(int(i) for i in my_string.replace(" - ", " + -").split(" + "))
