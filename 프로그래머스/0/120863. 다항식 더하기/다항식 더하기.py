1ed49492c18f9b68b9026a34c4cedccf7fd577b0
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-03-01 17:41:34 +0900

    [level 0] Title: 다항식 더하기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.py
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.py
@@ -1,23 +1,22 @@
 def solution(polynomial):
-    # 더하기를 기준으로 파싱
     ls = polynomial.split(" + ")
-
-    # 일차항과 상수항으로 분류
+    # 계수 분리를 위함
     coeff = 0
     const = 0
 
-    for s in ls:
-        if 'x' not in s:
-            const += int(s)
-        elif s == 'x':
+    # 분류 작업
+    for term in ls:
+        if term == 'x':
             coeff += 1
+        elif 'x' in term:
+            coeff += int(term[:-1])
         else:
-            coeff += int(s[:-1])
-    # 일차항 + 상수항, 일차항, 상수항으로 분류해서 리턴
+            const += int(term)
+    # 리턴 작업: 일차항 + 상수항, 일차항, 상수항
+    term = 'x' if coeff == 1 else str(coeff) + 'x'
     if coeff and const:
-        term = 'x' if coeff == 1 else str(coeff) + 'x'
         return term + ' + ' + str(const)
     elif const == 0:
-        return "x" if coeff == 1 else str(coeff) + "x"
+        return term
     elif coeff == 0:
         return str(const)
