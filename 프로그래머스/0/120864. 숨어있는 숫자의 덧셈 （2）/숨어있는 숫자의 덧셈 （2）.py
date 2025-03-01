32c6140341356469cc448a82da8db3b488af07cd
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-03-01 18:25:54 +0900

    Update 숨어있는 숫자의 덧셈 （2）.py

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -8,7 +8,7 @@ def solution(my_string):
         elif s.isdigit() is False and len(str_num):
             num += int(str_num)
             str_num = ""
-    if len(str_num):
+    if len(str_num): # 문자열에 숫자만 있는 경우
         num += int(str_num)
         return num
-    return num
+    return num # 문자열에 문자만 있는 경우 0으로 초기화했던 값을 리턴
