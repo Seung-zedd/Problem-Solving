7c697cdaa2ed63fcc8326d75b66053456a9e85cd
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-03-03 17:42:04 +0900

    [level 0] Title: 안전지대, Time: 0.10 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+def solution(board):
+    r = len(board)
+    c = len(board[0])
+    visited = [[False] * c for i in range(r)]
+    
+    # danger 카운팅
+    danger = 0
+
+    def danger_search(start_x, start_y):
+        visited[start_x][start_y] = True
+
+        # 좌상부터 시계방향으로 세팅
+        dr = [-1, -1, -1, 0, 1, 1, 1, 0]
+        dc = [-1, 0, 1, 1, 1, 0, -1, -1]
+
+        for i in range(8):
+            next_x = start_x + dr[i]
+            next_y = start_y + dc[i]
+
+            # 범위 설정
+            if 0 <= next_x <= r - 1 and 0 <= next_y <= c - 1:
+                if visited[next_x][next_y] == False and board[next_x][next_y] == 0:
+                    visited[next_x][next_y] = True
+    # board 순회
+    for i in range(r):
+        for j in range(c):
+            if board[i][j] == 1:
+                danger_search(i,j)
+    danger = sum(sum(row) for row in visited)
+    return r*c - danger
