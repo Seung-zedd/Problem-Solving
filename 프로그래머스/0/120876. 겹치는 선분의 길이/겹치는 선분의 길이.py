7342b1793d301ffdc6f7c998e18dffcc7cab4ba4
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-03-11 20:43:28 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -1,30 +1,26 @@
 def solution(lines):
-    def find_overlapped_lines():
+    def find_overlapped_line():
+        # (교정) 지역변수로 수정
         line = [(0, 1), (0, 2), (1, 2)]
         overlapped_line = []
-        # 임의의 2개 선분을 먼저 추출
         for u, v in line:
             a, b = lines[u]
             c, d = lines[v]
+            # end 부분은 포함 x(길이는 end - start이기 때문)
+            # (교정) 리스트끼리 &연산은 안되서 리스트 컴프리헨션 부분에서 set()으로 각각 설정해줘야함
+            line_a = set([i for i in range(a, b)])
+            line_b = set([i for i in range(c, d)])
 
-            # 직선을 set으로 표현 (종료점을 포함하지 않도록 수정)
-            line_a = set(i for i in range(a, b))
-            line_b = set(j for j in range(c, d))
-
-            # 겹치는 부분 저장
-            intersection = line_a & line_b
-            if intersection:
-                overlapped_line.append(intersection)
-
+            overlapped_line.append(line_a & line_b)
         return overlapped_line
 
-    intersect_set = find_overlapped_lines()
+    intersect_set = find_overlapped_line()
+    union_set = set()
 
-    # 겹치는 부분이 있을 경우 합집합 계산
-    if intersect_set:
-        union_set = set()
-        for s in intersect_set:
+    for s in intersect_set:
+        # 겹치는 부분이 있으면
+        if intersect_set:
             union_set |= s
-        return len(union_set)
-    else:
-        return 0
+        else:
+            return 0
+    return len(union_set)
