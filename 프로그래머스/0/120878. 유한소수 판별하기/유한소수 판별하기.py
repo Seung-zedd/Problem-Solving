6c407bab6b25ed1c02957a5cd2073ca79f56971e
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-03-14 13:38:43 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,25 +1,19 @@
 def solution(a, b):
-    # gcd(a, b)를 사용하여 먼저 최대공약수를 구함
     def gcd(n, d):
         if d == 0:
             return n
         return gcd(d, n % d)
-
     gcd_val = gcd(a, b)
-    # 기약분수로 만듦
-    b //= gcd_val
-    
-    # 분모가 1이면 항상 유한소수
-    if b == 1:
+    # 분수를 소수로 나타냄(float)
+    denom_factor = b // gcd_val
+
+    # a == b => gcd_val == b
+    # b == 1 => a/b는 항상 정수
+    if b == gcd_val or b == 1:
         return 1
-    
-    # 분모의 소인수가 2와 5만 존재하는지 확인
-    # 2로 나눌 수 있는 만큼 나눔
-    while b % 2 == 0:
-        b //= 2
-    # 5로 나눌 수 있는 만큼 나눔
-    while b % 5 == 0:
-        b //= 5
-    
-    # 만약 b가 1이 되었다면, 소인수가 2와 5만 존재했다는 의미
-    return 1 if b == 1 else 2
+    # 분모의 소인수가 2와 5만 존재하는지가 관건
+    while denom_factor % 2 == 0:
+        denom_factor //= 2
+    while denom_factor % 5 == 0:
+        denom_factor //= 5
+    return 1 if denom_factor == 1 else 2
