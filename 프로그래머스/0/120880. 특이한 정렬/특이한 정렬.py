efbc66c4452590ebe5c1ae682048dd9d1955d053
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-03-16 18:31:12 +0900

    [level 0] Title: 특이한 정렬, Time: 0.07 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
@@ -1,3 +1,19 @@
 def solution(numlist, n):
-    # Sort by distance (ascending), then by number (descending) for equal distances
-    return sorted(numlist, key=lambda x: (abs(x - n), -x))
+    num_dist = [(i, abs(n - i)) for i in numlist]  # [num, length]
+
+    # n과 가까운 거리를 기준으로 오름차순 정렬
+    len_sorted_ls = sorted(num_dist, key=lambda num: num[1])
+    # len_ls를 순회하는데 거리가 같으면 더 큰 수 앞에 배치
+    for idx, (num, dist) in enumerate(len_sorted_ls):
+        prev_dist = dist
+        # list index out of range 문제 핸들링
+        if idx == len(len_sorted_ls) - 1:
+            break
+        # 거리가 같고 작은 수가 앞에 있으면 스왑
+        # 거리가 같은데 이미 큰 수가 앞에 있으면 스왑 x
+        if prev_dist == len_sorted_ls[idx + 1][1] and len_sorted_ls[idx + 1][0] > num:
+            # 스왑
+            temp = len_sorted_ls[idx]
+            len_sorted_ls[idx] = len_sorted_ls[idx + 1]
+            len_sorted_ls[idx + 1] = temp
+    return [num for num, len in len_sorted_ls]
