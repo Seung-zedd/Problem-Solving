b11554d81159066a8e5bc6fa303744999b3a2ebb
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-03-18 17:19:53 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -1,6 +1,9 @@
 def solution(emergency):
-    # 내림차순으로 먼저 정렬 후, <K=value, V=idx + 1>로 딕셔너리 초기화
-    rank = {value: idx + 1 for idx, value in enumerate(sorted(emergency, reverse=True))}
-    # 배열 순회하면서 rank[value]로 리스트 컴프리헨션 사용
-    return [rank[x] for x in emergency]
-    # T.C: O(NlogN), using quick sort
+    # 1. 응급도가 높은 순서대로 먼저 정렬
+    emergency_sorted = sorted(emergency, reverse=True)
+    # 2. 그것을 기준으로 진료 순서를 매김
+    emergency_rank = {}
+    for i in emergency_sorted:
+        emergency_rank[i] = emergency_sorted.index(i) + 1
+    # 3. 진료 순서를 정한 배열을 리턴
+    return [emergency_rank[num] for num in emergency]
