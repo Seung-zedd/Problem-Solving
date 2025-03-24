43a6e180e6230a15fbf51846dafa95c1b4c81502
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-03-24 10:55:42 +0900

    [level 0] Title: 이진수 더하기, Time: 0.00 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.py
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.py
@@ -1,16 +1,2 @@
 def solution(bin1, bin2):
-    num1 = 0
-    num2 = 0
-    for idx, s in enumerate(bin1):
-        num1 += pow(2, len(bin1) - idx - 1) * int(s)
-    for idx, s in enumerate(bin2):
-        num2 += pow(2, len(bin2) - idx - 1) * int(s)
-
-    temp = num1 + num2
-    mod = ""
-    while 2 <= temp:
-        mod += str(temp % 2)
-        temp //= 2
-
-    result = mod + str(temp)
-    return result[::-1]
+    return bin(int(bin1, 2) + int(bin2, 2))[2:]
