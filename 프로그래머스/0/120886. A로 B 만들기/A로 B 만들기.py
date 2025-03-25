637d385689352db75008c650c6fb23932b50be3d
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-03-25 14:28:35 +0900

    [level 0] Title: A로 B 만들기, Time: 0.34 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
@@ -1,10 +1,2 @@
 def solution(before, after):
-    before_dict = {}  # {before: letter counting}
-    after_dict = {}
-    for s in before:
-        before_dict[s] = before_dict.get(s, 0) + 1
-    for s in after:
-        after_dict[s] = after_dict.get(s, 0) + 1
-
-    # 두 딕셔너리가 완전히 동일한지 비교
-    return 1 if before_dict == after_dict else 0
+    return 1 if sorted(before) == sorted(after) else 0
