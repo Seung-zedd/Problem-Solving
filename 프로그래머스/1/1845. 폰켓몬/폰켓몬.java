9a2bf4c67b822c0e458616571b093c52dec7f932
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-06-17 12:36:40 +0900

    [level 1] Title: 폰켓몬, Time: 4.97 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -4,14 +4,14 @@ class Solution {
     public int solution(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            set.add(nums[i]); // 중복이 되지 않기 때문에 그대로 저장
+        for (int num : nums) {
+            set.add(num);
         }
-        
-        // 종료 조건
-        if (set.size() <= nums.length / 2) {
+
+        if (set.size() < nums.length / 2) {
             return set.size();
         }
+
         return nums.length / 2;
     }
 }
