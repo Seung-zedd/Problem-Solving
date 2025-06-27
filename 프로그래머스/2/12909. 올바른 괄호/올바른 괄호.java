fca8e608080aac70133132b4064c0da4e4a6baff
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-06-27 14:28:09 +0900

    [level 2] Title: 올바른 괄호, Time: 26.67 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,16 +3,16 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            // String -> char로 바꿔서 하나씩 순회
-            char c = s.charAt(i);
 
+        // String -> char로 바꿔서 하나씩 순회
+        for (char c : s.toCharArray()) {
             if ((!stack.empty() && stack.peek() == '(') && c == ')') {
                 stack.pop();
             } else {
                 stack.push(c);
             }
         }
+        
         return stack.empty();
     }
 }
