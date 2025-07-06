5190710aa6202f7c2ef5a99965e54ff47b1da15f
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-07-06 18:19:16 +0900

    [level 2] Title: 프로세스, Time: 4.28 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,40 +2,38 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        // 대기 큐 초기화
+        // 큐 초기화 및 삽입
         Queue<Process> q = new LinkedList<>();
-        int count = 0;
-
-        // 큐에 우선순위 배열 삽입
         for (int i = 0; i < priorities.length; i++) {
             q.offer(new Process(priorities[i], i));
         }
 
-        // edge case
+        int count = 0;
+
+        // 엣지 케이스
         if (priorities.length == 1) {
             return count + 1;
         }
-        
-        // 규칙 적용 시작
+
+        // 규칙 시작
         while (!q.isEmpty()) {
-            // 규칙1: 대기 큐에 있는 프로세스를 꺼낸다.
+            // 규칙1
             Process curProc = q.poll();
             int curVal = curProc.priority;
             int curIdx = curProc.idx;
 
-            // 반복자를 통해 우선순위 최댓값 탑색
+            // 우선순위 최댓값 탐색
             int maxVal = 0;
-            Iterator<Process> maxIter = q.iterator();
-            //! for-each문은 ConcurrentModifiedException 터짐
-            while (maxIter.hasNext()) {
-                Process cur = maxIter.next();
-                int nextVal = cur.priority;
-                maxVal = Math.max(maxVal, nextVal);
+            Iterator<Process> iter = q.iterator();
+            while (iter.hasNext()) {
+                Process nextProc = iter.next();
+                int nextVal = nextProc.priority;
+                maxVal = Math.max(nextVal, maxVal);
             }
 
-            // 규칙2: 현재 프로세스보다 대기 큐에 더 우선순위가 높으면 다시 푸시
+            // 규칙2
             if (curVal < maxVal) {
-                q.offer(new Process(curVal, curIdx));
+                q.offer(curProc);
             } else {
                 count++;
                 if (curIdx == location) {
