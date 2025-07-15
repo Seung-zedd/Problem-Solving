747906ff949716b9aef1be5df9fd39d7696f9402
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-07-15 14:11:48 +0900

    [level 1] Title: 폰켓몬, Time: 8.43 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,7 +3,7 @@ import java.util.stream.*;
 
 class Solution {
     public int solution(int[] nums) {
-        Set<Integer> set = new HashSet<>(Arrays.stream(nums).boxed().collect(Collectors.toSet()));
+        Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
         return Math.min(set.size(), nums.length / 2);
     }
 }
