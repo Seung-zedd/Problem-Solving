a6051b2c4c2146562980e66cdfd3e94533e37157
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-07-15 14:08:06 +0900

    [level 2] Title: 의상, Time: 0.75 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,18 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        // {의상 종류, 개수}
+        // 해시 맵<의상의 종류, 개수> 초기화
         Map<String, Integer> map = new HashMap<>();
-        for (String[] clothe : clothes) {
-            map.merge(clothe[1], 1, Integer::sum);
+        for (String[] c : clothes) {
+            map.merge(c[1], 1, Integer::sum);
         }
-
-        // 여사건 이용: 전체 경우의 수 - 알몸
-        // 엔트리 맵을 통해 의상 종류 개수 계산
-        // return: 카티션 프로덕트(의상 개수 + 1) -  1
+        
+        // 여사건: 전체 경우의 수 - 1
+        // 카티션(map.values() + 1) - 1
         int answer = 1;
-        for (Map.Entry<String, Integer> e : map.entrySet()) {
-            answer *= e.getValue() + 1;
+        for (Integer i : map.values()) {
+            answer *= i + 1;
         }
 
         return answer - 1;
