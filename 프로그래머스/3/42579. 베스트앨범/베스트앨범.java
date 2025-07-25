36836a727384e606944d7078415bac8dd2ee04f7
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-07-25 14:25:08 +0900

    [level 3] Title: 베스트앨범, Time: 15.55 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,49 +2,54 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        // 장르 별 해시맵
         Map<String, Integer> map = new HashMap<>();
 
+        // 장르별 곡 먼저 map에 초기화
         for (int i = 0; i < genres.length; i++) {
             map.merge(genres[i], plays[i], Integer::sum);
         }
 
-        // 장르 별 내림차순 정렬
-        // sort()를 사용하기 위해 리스트로 초기화
-        List<Map.Entry<String, Integer>> sortedGenres = new ArrayList<>(map.entrySet());
-        sortedGenres.sort((e1, e2) -> Integer.compare(e2.getValue(), e1.getValue()));
+        // 장르별 내림차순 정렬
+        // map 자체로는 sort()를 사용 못하기 때문에 ArrayList로 초기화한 후 sort() 사용해서 내림차순 정렬
+        List<Map.Entry<String, Integer>> sortedGenre = new ArrayList<>(map.entrySet());
 
-        // 장르 내 재생횟수 내림차순 정렬하기 위해 List<Music>을 타입으로 지정
+        sortedGenre.sort((e1, e2) -> Integer.compare(e2.getValue(), e1.getValue()));
+
+        // 장르내 수록
+        //? 특정 장르 내에서 곡이 전부 다르기 때문에 List를 value로 설정
+        //! if Music 객체를 그냥 value로 사용하면 값이 덮어씌워지게 됨
         Map<String, List<Music>> musicMap = new HashMap<>();
+
         for (int i = 0; i < genres.length; i++) {
+            //? merge()는 사칙연산에 사용되기 때문에 computeIfAbsent()로 리스트를 추가해나감
             musicMap.computeIfAbsent(genres[i], k -> new ArrayList<>()).add(new Music(plays[i], i));
         }
 
-        // map -> musicMap으로 하기 위해 공통키인 sortedGenres의 key가 필요
-        List<Integer> answer = new ArrayList<>();
-        for (Map.Entry<String, Integer> e : sortedGenres) {
+        List<Integer> ans = new ArrayList<>();
+
+        // 장르 곡 -> 장르 내 수록
+        // 장르 곡의 key를 가져와서 장르 내 정렬 알고리즘 수행
+        //? 어짜피 map과 다른 musicMap에서 get() 연산을 하기 때문에 굳이 entrySet()으로 순회하지 않아도 됨
+        for (Map.Entry<String, Integer> e : sortedGenre) {
             String key = e.getKey();
             List<Music> musicList = musicMap.get(key);
+
             musicList.sort((m1, m2) ->
             {
-                // 재생횟수가 같으면 고유 번호 오름차순
                 if (m1.play == m2.play) {
                     return Integer.compare(m1.idx, m2.idx);
                 } else {
-                    // 그렇지 않으면 재생횟수 내림차순
                     return Integer.compare(m2.play, m1.play);
                 }
             });
 
-            // 베스트 앨범 2곡 수록
-            // 장르에 속한 곡이 하나라면, 하나의 곡만 선택하기 때문에 Math.min() 크기만큼 순회
             for (int i = 0; i < Math.min(2, musicList.size()); i++) {
-                answer.add(musicList.get(i).idx);
+                ans.add(musicList.get(i).idx);
             }
         }
-
-        // List -> int[]
-        return answer.stream().mapToInt(i -> i).toArray();
+        
+        // mapToInt()를 사용해서 Integer -> int로 언박싱
+        return ans.stream().mapToInt(i -> i).toArray();
     }
 
     private static class Music {
