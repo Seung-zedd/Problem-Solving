156e053c82116d17a1ddcf8d9836da0bb6dd5c14
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-08-12 15:58:45 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 23.27 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,34 +2,35 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        // 대기 큐
         Queue<Truck> waitingQ = new LinkedList<>();
-        // 대기 큐에 대기 트럭 배열 삽입
+
+        // 대기 큐에 트럭들 삽입
         for (int t : truck_weights) {
             waitingQ.offer(new Truck(t, 0));
         }
 
         // 다리 큐
         Queue<Truck> bridgeQ = new LinkedList<>();
+        int totalTruckWeight = 0;
 
-        // 시뮬레이션 종료 조건: 대기 큐에 트럭이 없고 && 다리 큐에 트럭이 없으면 종료
-        int time = 0; // 전체 시뮬레이션 시간
-        int totalTruckWeightOnBridge = 0;
+        // 시뮬레이션 시작
+        // 종료조건: 대기큐에 트럭이 없고 && 다리큐에 트럭이 없을 때
+        int time = 0;
         while (!waitingQ.isEmpty() || !bridgeQ.isEmpty()) {
             time++;
 
-            // 다리에 올라간 후
-            //* Settle And Act 적용(얘가 마지막에 있으면 시간이 흐른 후 다리에 올려야 하므로)
+            // Settle And Act(선 제거, 후 삽입)
             if (!bridgeQ.isEmpty() && time - bridgeQ.peek().entryTime == bridge_length) {
                 Truck exitTruck = bridgeQ.poll();
-                totalTruckWeightOnBridge -= exitTruck.weight;
+                totalTruckWeight -= exitTruck.weight;
             }
 
-            // 다리에 올리기 전
-            if (!waitingQ.isEmpty() && waitingQ.peek().weight + totalTruckWeightOnBridge <= weight && bridgeQ.size() < bridge_length) {
+            // 다리에 진입할 때
+            // 다리의 무게와 길이를 고려
+            if (!waitingQ.isEmpty() &&  waitingQ.peek().weight + totalTruckWeight <= weight && bridgeQ.size() < bridge_length) {
                 Truck entryTruck = waitingQ.poll();
                 bridgeQ.offer(new Truck(entryTruck.weight, time));
-                totalTruckWeightOnBridge += entryTruck.weight;
+                totalTruckWeight += entryTruck.weight;
             }
         }
 
@@ -38,7 +39,7 @@ class Solution {
 
     private static class Truck {
         int weight;
-        int entryTime; // 다리 진입시간
+        int entryTime;
 
         public Truck(int weight, int entryTime) {
             this.weight = weight;
