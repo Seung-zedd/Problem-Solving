0ac4cc957806015ed970c19c215017ca8eb2ed65
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-08-27 13:18:03 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.23 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -4,45 +4,25 @@ class Solution {
     public int[] solution(String[] wallpaper) {
         int m = wallpaper.length;
         int n = wallpaper[0].length();
+        
+        
+        int minR = Integer.MAX_VALUE;
+        int minC = Integer.MAX_VALUE;
+        int maxR = Integer.MIN_VALUE;
+        int maxC = Integer.MIN_VALUE;
 
-        // 먼저 완전 탐색으로 파일 개수 파악(어짜피 최댓값은 50이라 10^2이므로 쌉가능)
-        //* 수정: 파일의 좌표를 리스트에 추가
-
-        char[][] grid = new char[m][n];
-        // String 1차원 배열 -> char[] 2차원 배열(String은 객체 타입이라 인덱싱 접근이 안되므로)
+        // 완전 탐색 안에서 좌표의 극단값 찾기
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                char c = wallpaper[i].charAt(j);
-                grid[i][j] = c;
+                if (wallpaper[i].charAt(j) == '#') {
+                    minR = Math.min(minR, i);
+                    minC = Math.min(minC, j);
+                    maxR = Math.max(maxR, i);
+                    maxC = Math.max(maxC, j);
+                } 
             }
         }
 
-        // 완전 탐색으로 totalFile의 좌표를 일단 모음
-        // "모든 파일"을 제거해야 하기 위해 totalFile도 카운팅
-        int totalFile = 0;
-        List<Integer> rList = new ArrayList<>();
-        List<Integer> cList = new ArrayList<>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '#') {
-                    rList.add(i);
-                    cList.add(j);
-                    totalFile++;
-                }
-            }
-        }
-
-        // 좌표 오름차순으로 정렬
-        rList.sort(Integer::compare);
-        cList.sort(Integer::compare);
-
-
-        // 최솟값, 최댓값 추출
-        Integer minR = rList.get(0);
-        Integer minC = cList.get(0);
-        Integer maxR = rList.get(rList.size() - 1) + 1;
-        Integer maxC = cList.get(cList.size() - 1) + 1;
-
-        return new int[]{minR, minC, maxR, maxC};
+        return new int[]{minR, minC, maxR + 1, maxC + 1};
     }
 }
