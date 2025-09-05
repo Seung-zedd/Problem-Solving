7f3e063493b47ab118e5eee14bdae31c98d3a70e
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-09-05 18:26:05 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.29 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -2,16 +2,16 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] wallpaper) {
-        int m = wallpaper.length;
-        int n = wallpaper[0].length();
-        
-        
+        int m = wallpaper.length; // 행
+        int n = wallpaper[0].length(); // 열
+
+        // 완전 탐색을 하면서 좌표값 모음
+        // 모든 파일들의 좌표를 포함하는 최솟값 좌표와 최댓값 좌표를 모으기 위함
         int minR = Integer.MAX_VALUE;
         int minC = Integer.MAX_VALUE;
         int maxR = Integer.MIN_VALUE;
         int maxC = Integer.MIN_VALUE;
 
-        // 완전 탐색 안에서 좌표의 극단값 찾기
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (wallpaper[i].charAt(j) == '#') {
@@ -19,10 +19,12 @@ class Solution {
                     minC = Math.min(minC, j);
                     maxR = Math.max(maxR, i);
                     maxC = Math.max(maxC, j);
-                } 
+                }
             }
         }
 
+        // 모은 좌표값을 배열로 리턴
         return new int[]{minR, minC, maxR + 1, maxC + 1};
+
     }
 }
