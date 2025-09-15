559e15cdaf90a9d7b0c69aa9a610e9afb2f1e15e
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-09-15 21:03:10 +0900

    [level 1] Title: 실패율, Time: 27.96 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,34 +1,40 @@
 import java.util.*;
 
-public class Solution {
+class Solution {
     public int[] solution(int N, int[] stages) {
-        // 1. 스테이지별 도전자 수를 구함
-        int[] challenger = new int[N + 2]; // 인덱스를 스테이지 번호에 맞추기 위해서(1 <= x <= N+1)
+        // 1. 도전자의 수 계산
+        // 0~N ->1~N+1 && N단계까지 클리어한 수
+        int[] challenger = new int[N + 2];
+
+        // stage 번호에 따른 도전자의 수 1씩 증가
         for (int stage : stages) {
-            challenger[stage] += 1; // stages의 val이 현재 스테이지에 멈춰있는 도전자 수
+            challenger[stage] += 1;
         }
 
-        // 2. 스테이지별 실패한 사용자 수 계산
+        // 실패율 계산
         Map<Integer, Double> failRate = new HashMap<>();
-        double total = stages.length; // 여사건을 이용하기 위함
+        double total = stages.length;
 
-        // 3. 각 스테이지를 순회하며, 실패율 계산
+        // i는 스테이지 번호에 맞춘다.
         for (int i = 1; i < N + 1; i++) {
-            // 도전한 사람이 없는 경우, 실패율은 0
+            // edge case: 스테이지에 도달한 유저가 없는 경우 해당 스테이지의 실패율은 0 으로 정의한다.
             if (challenger[i] == 0) {
                 failRate.put(i, 0.0);
             } else {
                 failRate.put(i, challenger[i] / total);
-                total -= challenger[i]; // 여사건 적용
+                total -= challenger[i];
             }
         }
-        
-        // 4. 실패율이 높은 스테이지부터 내림차순 정렬
-        // 실패율이 같으면 스테이지 번호 오름차순 정렬
-        return failRate.entrySet()
+
+        // 실패율이 높은 스테이지부터 내림차순
+        // if (실패율이 같은 스테이지가 있다면): 작은 번호의 스테이지가 먼저 오도록 하면 된다.
+        List<Map.Entry<Integer, Double>> sortedList = new ArrayList<>(failRate.entrySet());
+
+        // 삼항 연산자
+        return sortedList
                 .stream()
-                .sorted((o1, o2) ->
-                        o1.getValue().equals(o2.getValue()) ? Integer.compare(o1.getKey(), o2.getKey()) : Double.compare(o2.getValue(), o1.getValue()))
-                .mapToInt(Map.Entry::getKey).toArray();
+                .sorted((s1, s2) ->
+                        s1.getValue().equals(s2.getValue()) ? Integer.compare(s1.getKey(), s2.getKey()) : Double.compare(s2.getValue(), s1.getValue())
+                ).mapToInt(Map.Entry::getKey).toArray();
     }
 }
