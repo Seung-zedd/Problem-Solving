256623fcb57b49d329172e1b06d5f9d0e763d40e
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-09-18 11:56:53 +0900

    [level 2] Title: 행렬의 곱셈, Time: 7.86 ms, Memory: 93.1 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/README.md
+++ 프로그래머스/2/12949. 행렬의 곱셈/README.md
@@ -1,10 +1,10 @@
 # [level 2] 행렬의 곱셈 - 12949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949) 
 
 ### 성능 요약
 
-메모리: 96.5 MB, 시간: 11.82 ms
+메모리: 93.1 MB, 시간: 7.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 11일 10:17:21
+2025년 09월 18일 11:56:48
 
 ### 문제 설명
 
--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,23 +1,21 @@
-import java.util.*;
-
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        // 1. 행렬의 행과 열을 변수에 저장
-        // (m x k) x (k x n) = m x n
+        // 2차원 배열을 보면 제일 먼저 row, col을 변수에 저장
         int r1 = arr1.length;
         int c1 = arr1[0].length;
         int r2 = arr2.length;
         int c2 = arr2[0].length;
 
-        // 행렬 크기 초기화
+        // (m x k) x (k x n) = m x n
+        // k는 공유 차원에 참여하는 열벡터
+
+        // 최종 곱해진 행렬 크기는 r1 x c2
         int[][] answer = new int[r1][c2];
 
-        // 2. 행렬의 곱을 반복문으로 실행
         for (int i = 0; i < r1; i++) {
             for (int j = 0; j < c2; j++) {
                 for (int k = 0; k < c1; k++) {
-                    // 행벡터 x 열벡터
-                    answer[i][j] += arr1[i][k] *arr2[k][j];
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
                 }
             }
         }
