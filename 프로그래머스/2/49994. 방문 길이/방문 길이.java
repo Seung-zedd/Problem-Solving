a7bd646e88872f5dd958d09a71ed441a59107520
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-09-22 17:06:14 +0900

    [level 2] Title: 방문 길이, Time: 18.49 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,8 +1,33 @@
 import java.util.*;
 
 class Solution {
-    private static boolean isValidMove(int nx, int ny) {
-        return -5 <= nx && nx <= 5 && -5 <= ny && ny <= 5;
+    public int solution(String dirs) {
+        // 캐릭터는 원점부터 시작
+        int x = 0, y = 0;
+
+        // 명령어 -> 좌표이동으로 매핑
+        initLocation();
+
+        // 명령어 순회
+        for (int i = 0; i < dirs.length(); i++) {
+            int[] offset = location.get(dirs.charAt(i));
+            int nx = x + offset[0];
+            int ny = y + offset[1];
+
+            // 좌표평면 범위를 통과하면
+            if (isValidMove(nx, ny)) {
+                edges.add(x + "," + y + " " + nx + "," + ny);
+                edges.add(nx + "," + ny + " " + x + "," + y);
+
+                // 좌표 업데이트
+                x = nx;
+                y = ny;
+            }
+        }
+
+        return edges.size() / 2;
+
+
     }
 
     private static final Map<Character, int[]> location = new HashMap<>();
@@ -14,30 +39,9 @@ class Solution {
         location.put('R', new int[]{1, 0});
     }
 
-
-    public int solution(String dirs) {
-        initLocation();
-        int x = 0, y = 0; // 원점
-
-        // 중복 경로를 제거하기 위함
-        Set<String> edges = new HashSet<>();
-
-        // 명령어 순회
-        for (int i = 0; i < dirs.length(); i++) {
-            int[] offset = location.get(dirs.charAt(i));
-            int nx = x + offset[0];
-            int ny = y + offset[1];
-
-            if (isValidMove(nx, ny)) {
-                edges.add(x + "," + y + " " + nx + "," + ny);
-                edges.add(nx + "," + ny + " " + x + "," + y);
-                
-                // 좌표 업데이트
-                x = nx;
-                y = ny;
-            }
-        }
-
-        return edges.size() / 2;
+    private static boolean isValidMove(int nx, int ny) {
+        return -5 <= nx && nx <= 5 && -5 <= ny && ny <= 5;
     }
+
+    private static Set<String> edges = new HashSet<>();
 }
