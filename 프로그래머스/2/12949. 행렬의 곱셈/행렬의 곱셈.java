d3c503ee36e824ca70264eac986ad6ed58ecd1e3
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-09-28 20:24:56 +0900

    [level 2] Title: 행렬의 곱셈, Time: 11.01 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,15 +1,13 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        // 2차원 배열을 보면 제일 먼저 row, col을 변수에 저장
+        // row와 col을 변수에 저장한다
         int r1 = arr1.length;
         int c1 = arr1[0].length;
         int r2 = arr2.length;
         int c2 = arr2[0].length;
-
+        
+        // 행렬의 곱셈
         // (m x k) x (k x n) = m x n
-        // k는 공유 차원에 참여하는 열벡터
-
-        // 최종 곱해진 행렬 크기는 r1 x c2
         int[][] answer = new int[r1][c2];
 
         for (int i = 0; i < r1; i++) {
