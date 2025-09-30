a6d942e924ce64af712c35a9a504d88891712dd6
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-09-30 20:44:24 +0900

    [level 2] Title: 방문 길이, Time: 20.80 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,47 +1,48 @@
 import java.util.*;
 
 class Solution {
-    public int solution(String dirs) {
-        // 캐릭터는 원점부터 시작
-        int x = 0, y = 0;
-
-        // 명령어 -> 좌표이동으로 매핑
-        initLocation();
-
-        // 명령어 순회
-        for (int i = 0; i < dirs.length(); i++) {
-            int[] offset = location.get(dirs.charAt(i));
-            int nx = x + offset[0];
-            int ny = y + offset[1];
-
-            // 좌표평면 범위를 통과하면
-            if (isValidMove(nx, ny)) {
-                edges.add(x + "," + y + " " + nx + "," + ny);
-                edges.add(nx + "," + ny + " " + x + "," + y);
-
-                // 좌표 업데이트
-                x = nx;
-                y = ny;
-            }
-        }
-
-        return edges.size() / 2;
-
-
-    }
-
-    private static final Map<Character, int[]> location = new HashMap<>();
+    // 명령어를 좌표로 매핑
+    // 불변 컬렉션은 solution 외부에서 초기화
+    private static final Map<Character, int[]> map = new HashMap<>();
 
     private static void initLocation() {
-        location.put('U', new int[]{0, 1});
-        location.put('D', new int[]{0, -1});
-        location.put('L', new int[]{-1, 0});
-        location.put('R', new int[]{1, 0});
+        map.put('U', new int[]{0, 1});
+        map.put('D', new int[]{0, -1});
+        map.put('L', new int[]{-1, 0});
+        map.put('R', new int[]{1, 0});
     }
 
     private static boolean isValidMove(int nx, int ny) {
         return -5 <= nx && nx <= 5 && -5 <= ny && ny <= 5;
     }
 
-    private static Set<String> edges = new HashSet<>();
+
+    public int solution(String dirs) {
+        initLocation();
+
+        // 원점에서 시작
+        int x = 0, y = 0;
+
+        // path: 시점~종점 간의 경로로 해석 && 중복을 제거하기 위해 해시셋으로 초기화
+        Set<String> edges = new HashSet<>();
+
+        // 명령어에 따른 이동 시작
+        for (int i = 0; i < dirs.length(); i++) {
+            int[] offset = map.get(dirs.charAt(i));
+            int nx = x + offset[0];
+            int ny = y + offset[1];
+
+
+            // 경계에 포함되면
+            if (isValidMove(nx, ny)) {
+                edges.add(x + "," + y + " " + nx + "," + ny);
+                edges.add(nx + "," + ny + " " + x + "," + y);
+                // 이동을 했으니까 좌표 업데이트
+                x = nx;
+                y = ny;
+            }
+        }
+
+        return edges.size() / 2;
+    }
 }
