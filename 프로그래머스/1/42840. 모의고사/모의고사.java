bf386b9d2012bfef9a52dc8185f78cdc49fd5e57
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-10-07 18:44:12 +0900

    [level 1] Title: 모의고사, Time: 5.07 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,19 +1,20 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] answers) {
-        // 수포자 패턴 분석
-        int[][] patterns = {
-                {1, 2, 3, 4, 5},
-                {2, 1, 2, 3, 2, 4, 2, 5},
-                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
-        };
+    // 읽기 전용 2차원 배열
+    private static final int[][] patterns = {
+            {1, 2, 3, 4, 5},
+            {2, 1, 2, 3, 2, 4, 2, 5},
+            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+    };
 
-        // 정답 카운팅하기 위한 scores 배열 초기화
+    public int[] solution(int[] answers) {
+        // 수포자 정답 저장용
         int[] scores = new int[3];
 
-        // 패턴과 정답 비교
         for (int i = 0; i < answers.length; i++) {
+            // 수포자의 답들을 정답과 순서대로 비교
+            // j는 row
             for (int j = 0; j < patterns.length; j++) {
                 if (answers[i] == patterns[j][i % patterns[j].length]) {
                     scores[j]++;
@@ -21,18 +22,17 @@ class Solution {
             }
         }
 
-        int maxVal = Integer.MIN_VALUE;
+        int maxScore = Integer.MIN_VALUE;
 
         // 최고 점수 업데이트
-        for (int s : scores) {
-            maxVal = Math.max(maxVal, s);
+        for (int score : scores) {
+            maxScore = Math.max(maxScore, score);
         }
 
-        // 가장 높은 점수를 리턴하기 위한 리스트
         List<Integer> numList = new ArrayList<>();
 
         for (int i = 0; i < scores.length; i++) {
-            if (scores[i] == maxVal) {
+            if (scores[i] == maxScore) {
                 numList.add(i + 1);
             }
         }
