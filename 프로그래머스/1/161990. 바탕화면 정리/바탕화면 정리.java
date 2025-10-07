487a4109e4e9985becf5ea237c96a44325cf483e
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-10-07 18:29:31 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.28 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,23 +1,23 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(String[] wallpaper) {
-        int minRow = Integer.MAX_VALUE;
-        int minCol = Integer.MAX_VALUE;
-        int maxRow = Integer.MIN_VALUE;
-        int maxCol = Integer.MIN_VALUE;
-
+        int minR = Integer.MAX_VALUE;
+        int minC = Integer.MAX_VALUE;
+        int maxR = Integer.MIN_VALUE;
+        int maxC = Integer.MIN_VALUE;
+        
+        // i = row, j = col
         for (int i = 0; i < wallpaper.length; i++) {
             for (int j = 0; j < wallpaper[i].length(); j++) {
                 if (wallpaper[i].charAt(j) == '#') {
-                    minRow = Math.min(minRow, i);
-                    minCol = Math.min(minCol, j);
-                    maxRow = Math.max(maxRow, i);
-                    maxCol = Math.max(maxCol, j);
+                    // 좌표 최빈값 저장
+                    minR = Math.min(minR, i);
+                    minC = Math.min(minC, j);
+                    maxR = Math.max(maxR, i);
+                    maxC = Math.max(maxC, j);
                 }
             }
         }
 
-        return new int[]{minRow, minCol, maxRow + 1, maxCol + 1};
+        return new int[]{minR, minC, maxR + 1, maxC + 1};
     }
 }
