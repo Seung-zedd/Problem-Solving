fcd6aafefca8f763849b6fe0c6572e1278e68237
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-10-11 16:18:07 +0900

    [level 2] Title: 방문 길이, Time: 16.92 ms, Memory: 96 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,10 +1,10 @@
 import java.util.*;
 
 class Solution {
-    // 명령어를 좌표로 매핑
-    // 불변 컬렉션은 solution 외부에서 초기화
+
     private static final Map<Character, int[]> map = new HashMap<>();
 
+    // 명령어 좌표에 매핑
     private static void initLocation() {
         map.put('U', new int[]{0, 1});
         map.put('D', new int[]{0, -1});
@@ -12,35 +12,33 @@ class Solution {
         map.put('R', new int[]{1, 0});
     }
 
-    private static boolean isValidMove(int nx, int ny) {
-        return -5 <= nx && nx <= 5 && -5 <= ny && ny <= 5;
+    private static boolean isValidMove(int x, int y) {
+        return -5 <= x && x <= 5 && -5 <= y && y <= 5;
     }
-
 
     public int solution(String dirs) {
         initLocation();
 
-        // 원점에서 시작
+        // 캐릭터 위치 설정
         int x = 0, y = 0;
 
-        // path: 시점~종점 간의 경로로 해석 && 중복을 제거하기 위해 해시셋으로 초기화
+        // 캐릭터가 처음 걸어본 길의 길이를 구하기 위한 해시셋
+        // 두 정점을 저장해서 식별
         Set<String> edges = new HashSet<>();
 
-        // 명령어에 따른 이동 시작
         for (int i = 0; i < dirs.length(); i++) {
             int[] offset = map.get(dirs.charAt(i));
             int nx = x + offset[0];
             int ny = y + offset[1];
 
-
-            // 경계에 포함되면
+            // 좌표 경계에 포함되면
             if (isValidMove(nx, ny)) {
                 edges.add(x + "," + y + " " + nx + "," + ny);
                 edges.add(nx + "," + ny + " " + x + "," + y);
-                // 이동을 했으니까 좌표 업데이트
+                // 경계에 포함되서 이동했으니까 좌표 업데이트
                 x = nx;
                 y = ny;
-            }
+            } 
         }
 
         return edges.size() / 2;
