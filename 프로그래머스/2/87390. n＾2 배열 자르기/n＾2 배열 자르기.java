5fc744b0536d62e19a429a1114955fe8dd22eac6
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-10-14 18:48:46 +0900

    [level 2] Title: n^2 배열 자르기, Time: 7.88 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -1,15 +1,17 @@
 class Solution {
     public int[] solution(int n, long left, long right) {
-        // left <= x <= right의 닫힌 구간
+        // 1차원 배열 생성
         int[] answer = new int[(int) (right - left + 1)];
 
         for (int i = 0; i < answer.length; i++) {
-            long k = i + left; // 인덱스 k 설정
+            // 포인터 생성
+            long k = left + i;
+
+            // row, col 공식
+            //! Java에서 long / int 연산을 하면 int가 자동으로 long으로 형변환
             int row = (int) (k / n);
             int col = (int) (k % n);
 
-            // 값을 채움(zero-indexed 기준)
-            // 대각성분은 row, col이 같고 i = 1부터 시작하기 때문에 1을 더함
             answer[i] = Math.max(row, col) + 1;
         }
 
