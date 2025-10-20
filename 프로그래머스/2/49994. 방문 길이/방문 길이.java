5f436f3f47baae9a03e6621b67cfbc919ab5c66f
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-10-20 13:31:06 +0900

    [level 2] Title: 방문 길이, Time: 20.42 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,44 +1,41 @@
 import java.util.*;
 
 class Solution {
+    private static Map<Character, int[]> map = new HashMap<>();
 
-    private static final Map<Character, int[]> map = new HashMap<>();
-
-    // 명령어 좌표에 매핑
     private static void initLocation() {
-        map.put('U', new int[]{0, 1});
-        map.put('D', new int[]{0, -1});
-        map.put('L', new int[]{-1, 0});
-        map.put('R', new int[]{1, 0});
+        map.put('U', new int[]{1, 0});
+        map.put('D', new int[]{-1, 0});
+        map.put('L', new int[]{0, -1});
+        map.put('R', new int[]{0, 1});
     }
 
-    private static boolean isValidMove(int x, int y) {
-        return -5 <= x && x <= 5 && -5 <= y && y <= 5;
+    private static boolean isValidMove(int nx, int ny) {
+        return -5 <= nx && nx <= 5 && -5 <= ny && ny <= 5;
     }
-
     public int solution(String dirs) {
+        // 명령어 초기화
         initLocation();
 
-        // 캐릭터 위치 설정
+        // 캐릭터는 원점에서 시작
         int x = 0, y = 0;
-
-        // 캐릭터가 처음 걸어본 길의 길이를 구하기 위한 해시셋
-        // 두 정점을 저장해서 식별
+        // 두 정점을 저장하기 위함
         Set<String> edges = new HashSet<>();
 
+        // 명령어 파싱
         for (int i = 0; i < dirs.length(); i++) {
             int[] offset = map.get(dirs.charAt(i));
             int nx = x + offset[0];
             int ny = y + offset[1];
 
-            // 좌표 경계에 포함되면
+            // 좌표경계 안에 포함되면
             if (isValidMove(nx, ny)) {
                 edges.add(x + "," + y + " " + nx + "," + ny);
                 edges.add(nx + "," + ny + " " + x + "," + y);
-                // 경계에 포함되서 이동했으니까 좌표 업데이트
+                // 좌표 업데이트
                 x = nx;
                 y = ny;
-            } 
+            }
         }
 
         return edges.size() / 2;
