cc75c440ec0e5b49ac67e21da02e087c51fcadf9
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-10-25 21:11:13 +0900

    [level 2] Title: 짝지어 제거하기, Time: 151.61 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s) {
+        // 짝 제거
+        return deletePair(s) ? 1 : 0;
+
+
+    }
+
+    private static boolean deletePair(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
