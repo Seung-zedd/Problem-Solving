4f26d7c03e06cd0ef06544f8c8dfbc318ead5bfc
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2025-10-29 15:00:30 +0900

    [level 2] Title: 주식가격, Time: 38.45 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -15,9 +15,7 @@ class Solution {
             // 현재 가격보다 과거의 가격이 높으면
             //! while문으로 처리해야 하는 이유: 스택에 남아있는 원소들이 현재 가격보다 계속 높을 수 있기 때문
             while (!s.isEmpty() && s.peek()[0] > prices[i]) {
-                int prevSec = s.pop()[1];
-                int term = i - prevSec;
-                answer[prevSec] = term;
+                calculateTerm(s, i, answer);
             }
 
             s.push(new int[]{prices[i], i});
@@ -25,11 +23,15 @@ class Solution {
 
         // 남아있는 스택을 처리, 즉 가격이 떨어지지 않은 주식가격들의 기간을 계산
         while (!s.isEmpty()) {
-            int prevSec = s.pop()[1];
-            int term = (prices.length - 1) - prevSec;
-            answer[prevSec] = term;
+            calculateTerm(s, (prices.length - 1), answer);
         }
 
         return answer;
+    }
+
+    private static void calculateTerm(Stack<int[]> s, int length, int[] answer) {
+        int prevSec = s.pop()[1];
+        int term = length - prevSec;
+        answer[prevSec] = term;
     }
 }
