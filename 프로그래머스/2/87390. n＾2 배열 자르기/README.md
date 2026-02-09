a280625aad62a1a2a4e082520981f267d7719e69
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2026-02-09 18:45:57 +0900

    [level 2] Title: n^2 배열 자르기, Time: 7.55 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/README.md
+++ 프로그래머스/2/87390. n＾2 배열 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] n^2 배열 자르기 - 87390 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390?language=java) 
 
 ### 성능 요약
 
-메모리: 111 MB, 시간: 7.88 ms
+메모리: 91.7 MB, 시간: 7.55 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 14일 18:48:40
+2026년 02월 09일 18:45:51
 
 ### 문제 설명
 
--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -1,20 +1,22 @@
 class Solution {
     public int[] solution(int n, long left, long right) {
-        // 1차원 배열 생성
-        int[] answer = new int[(int) (right - left + 1)];
+        // 1) 1차원 배열의 크기를 미리 초기화
+        int len = (int)(right - left) + 1;
+        int[] arr = new int[len];
 
-        for (int i = 0; i < answer.length; i++) {
-            // 포인터 생성
-            long k = left + i;
-
-            // row, col 공식
-            //! Java에서 long / int 연산을 하면 int가 자동으로 long으로 형변환
-            int row = (int) (k / n);
+        // 2) left ~ right 사이를 움직이는 인덱스를 k라고 하자.
+        for (int i = 0; i < len; i++) {
+            long k = left + i; // 파라미터로 주어진 left, right가 long 타입이기 때문에 움직이고자 하는 인덱스 k도 long 타입으로 초기화해야함
+            // 조건1 1 ≤ n ≤ 10^7과 조건2 0 ≤ left ≤ right < n^2를 조합하면 인덱스 최대치인 right가 n보다는 크다는 것을 의미
+            // 따라서, row, col을 구할 때는 k를 크게 잡는다.
+            int row = (int)(k / n);
             int col = (int) (k % n);
 
-            answer[i] = Math.max(row, col) + 1;
+            // 빈칸에 채워야하는 원소는 max(row, col) + 1 (row와 col을 배열처럼 0부터 시작한다고 했을 때)
+            //! 애초에 row와 col을 0부터 시작하면 0부터 시작하는 인덱스 k를 가진 1차원 배열과 offset을 맞출 수가 있다!
+            arr[i] = Math.max(row, col) + 1;
         }
-
-        return answer;
+        
+        return arr; 
     }
 }
