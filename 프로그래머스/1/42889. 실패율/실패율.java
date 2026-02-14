c5cb299bdf3a3f073725d0ab2dec7c5a2d6b83ec
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2026-02-14 12:23:08 +0900

    [level 1] Title: 실패율, Time: 15.61 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,35 +1,49 @@
+
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
-        // 0은 더미 슬롯, N+1 도전자 수도 포함
+        
+        
+        // 1번 매핑
         int[] challengers = new int[N + 2];
-
         for (int s : stages) {
-            challengers[s] += 1;
+            challengers[s]++;
         }
 
-        // 실패율 계산
-        Map<Integer, Double> failMap = new HashMap<>();
-        int total = stages.length;
-
+        // 2번 매핑
+        Map<Integer, Double> failureMap = new HashMap<>();
+        double total = stages.length;
         for (int i = 1; i < N + 1; i++) {
-            // 스테이지에 도달한 유저가 없는 경우 해당 스테이지의 실패율은 0 으로 정의
-            if (challengers[i] == 0) {
-                failMap.put(i, 0.);
+            if (total == 0) {
+                failureMap.put(i, 0.0);
             } else {
-                double failRate = (double) challengers[i] / total;
-                failMap.put(i, failRate);
-                total -= challengers[i]; // 스테이지 레벨을 올리기 전에 이전에 도달한 스테이지 사람들을 제외
+                failureMap.put(i, (double) challengers[i] / total);
+                total -= challengers[i];
             }
         }
 
-        // 실패율이 높은 스테이지부터 내림차순
-        // 만약 실패율이 같은 스테이지가 있다면 스테이지 번호 오름차순(stable)
-        return failMap.entrySet()
-                .stream()
-                .sorted((e1, e2) -> e1.getValue().equals(e2.getValue()) ? Integer.compare(e1.getKey(), e2.getKey()) : Double.compare(e2.getValue(), e1.getValue()))
-                .mapToInt(Map.Entry::getKey).toArray();
+        // 3번 매핑
+        List<Map.Entry<Integer, Double>> entryList = new ArrayList<>(failureMap.entrySet());
+
+        entryList.sort((e1, e2) -> {
+            // 실패율 내림차순
+            int res = Double.compare(e2.getValue(), e1.getValue());
+
+            // 실패율이 같다면 스테이지 번호 오름차순 정렬
+            // ! 중괄호 블럭은 실행 블록이라고 컴파일러가 판단하기 때문에 반드시 리턴값을 명시해줘야함!
+            if (res == 0) {
+                return Integer.compare(e1.getKey(), e2.getKey());
+            } else {
+                return res;
+            }
+        });
+
+        // ? [문법 단서 1] Comparator의 리턴: 음수는 "그대로", 양수는 "바꾸기", 0은 "같음"을 신호한다.
+        // ? [문법 단서 2] mapToInt(Map.Entry::getKey): 엔트리 상자에서 키(스테이지 번호)만 쏙 빼내는 마법!
+        // ! 주의: 나누기 연산 시 피연산자 중 최소 하나는 double이어야 소수점이 보존된다는 것을 잊지 말자.
+
+        return entryList.stream().mapToInt(Map.Entry::getKey).toArray();
+    
     }
 }
