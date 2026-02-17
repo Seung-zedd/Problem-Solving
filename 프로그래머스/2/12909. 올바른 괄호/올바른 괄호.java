6bddf6534968cc96d82608ebad56bc694a0ed8c3
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2026-02-17 16:37:55 +0900

    [level 2] Title: 올바른 괄호, Time: 31.80 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,16 +2,23 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        Stack<Character> stack = new Stack<>();
-        // 괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다는 뜻
+        Stack<Character> st = new Stack<>();
+
+        // s 순회
         for (int i = 0; i < s.length(); i++) {
-            if (!stack.isEmpty() && s.charAt(i) == ')') {
-                stack.pop();
+            Character c = s.charAt(i);
+
+            // 괄호가 짝지어져있으면 팝
+            if (!st.isEmpty() && st.peek() == '(' && c == ')') {
+                st.pop();
+                // 닫힌 괄호가 먼저 오면 false
+            } else if (st.isEmpty() && c == ')') {
+                return false;
             } else {
-                stack.push(s.charAt(i));
+                st.push(c);
             }
         }
 
-        return stack.isEmpty();
+        return st.empty();
     }
 }
