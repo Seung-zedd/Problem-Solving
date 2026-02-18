4ac9429e9c7f5fb365df27bf20ce4f02bca78c86
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2026-02-18 16:31:05 +0900

    [level 2] Title: 괄호 회전하기, Time: 62.23 ms, Memory: 98.8 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,44 +2,39 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-        int count = 0;
-        // s를 먼저 순회
+        int answer = 0;
+        // 1. s를 0 ~ s.len - 1 만큼 순회하기 위해 for문 사용
         for (int i = 0; i < s.length(); i++) {
-            // s를 i만큼 회전
-             count += rotateWithLeft(s, i);
+            String str = leftLotate(i, s);
+            answer += isValid(str);
         }
 
-        return count;
+        return answer;
     }
 
-    private static int rotateWithLeft(String s, int i) {
-        String result = s.substring(i) + s.substring(0, i);
-
-        return isValidParenthesis(result) ? 1 : 0;
+    private static String leftLotate(int x, String s) {
+        // x만큼 왼쪽으로 회전: x부터 끝까지 + 처음부터 x전까지
+        return s.substring(x) + s.substring(0, x);
     }
 
-    private static boolean isValidParenthesis(String str) {
-        Stack<Character> s = new Stack<>();
+    private static int isValid(String s) {
+        Stack<Character> st = new Stack<>();
 
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            // 열린 괄호면
-            if (c == '(' || c == '{' || c == '[') {
-                s.push(c);
+        for (int i = 0; i < s.length(); i++) {
+            Character c = s.charAt(i);
+            // 괄호 짝 매칭: !st.isEmpty()가 모든 || 조건을 보호하도록 괄호로 묶음
+            //! 연산자 순서가 애매하면 괄호로 안전하게 묶자!
+            if (!st.isEmpty() && ((st.peek() == '(' && c == ')') ||
+                    (st.peek() == '{' && c == '}') ||
+                    (st.peek() == '[' && c == ']'))) {
+                st.pop();
+            } else if (c == ')' || c == '}' || c == ']') {
+                // 짝이 맞지 않는 닫힌 괄호가 오거나 스택이 비어있는데 닫힌 괄호가 온 경우
+                return 0;
             } else {
-                // 스택이 비지 않았을 때 짝을 맞춘다
-                if (!s.isEmpty()) {
-                    if (s.peek() == '(' && c == ')' || s.peek() == '{' && c == '}' || s.peek() == '[' && c == ']') {
-                        s.pop();
-                    }
-                } else {
-                    //닫힌 괄호가 나오면 무조건 false
-                    return false;
-                }
+                st.push(c);
             }
         }
-
-        return s.isEmpty();
+        return st.isEmpty() ? 1 : 0;
     }
-
-    }
+}
