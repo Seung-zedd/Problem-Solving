1d51759c41971b09fdcf1bf2beed2ebf884299fe
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2026-02-22 11:26:09 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.31 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -2,42 +2,44 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        // 1. 바구니(스택) 초기화
-        Stack<Integer> s = new Stack<>();
-        int r = board.length;
-        int c = board[0].length;
+        int count = 0;
 
-        // 2. 크레인을 먼저 움직임
-        // moves의 원소가 크레인의 위치를 의미하므로 for-each문으로 순회
-        int answer = 0;
+        // 1. board의 row, col 추출
+        int r = board.length;
+
+        // 바구니 소환
+        Stack<Integer> s = new Stack<>();
+
+        // 2. 크레인 작동
+        // moves[i]는 해당 board의 col이므로 고정시켜야함
+        // 크레인은 한번만 작동 -> 스택에 담았으면 break로 다음 m 순회
+        // 10^6 < 10^8이므로 이중 for문 가능!
         for (int m : moves) {
-            // row를 순회
             for (int i = 0; i < r; i++) {
+                // 만약 인형이 없는 곳에서 크레인을 작동시키는 경우에는 아무런 일도 일어나지 않습니다.
+                // moves의 길이가 1부터 시작하므로 zero-based로 맞춘다
                 if (board[i][m - 1] == 0) {
                     continue;
                 } else {
                     s.push(board[i][m - 1]);
-                    // 같은 원소의 값이 존재하는지 검증
-                    answer += AfterExploding(s);
-                    board[i][m - 1] = 0; // 인형 꺼냄
-                    break; // moves는 1번만 움직이기 때문
+                    board[i][m - 1] = 0;
+                    count += afterExploding(s);
+                    break;
                 }
             }
         }
 
-        return answer;
+        return count;
     }
 
-    private static int AfterExploding(Stack<Integer> s) {
-        // stack의 top 이전 원소와 현재 들어가고 있는 값을 비교
-        if (!s.isEmpty() && (s.size() > 1 && (s.get(s.size() - 2) == s.peek()))) {
-            // 2개를 터뜨림
+    private static int afterExploding(Stack<Integer> s) {
+        // stack 안에서 if (stack[top - 1] == stack[top]): stack.pop()을 푸시할 때마다 검증
+        if (s.size() > 1 && (s.get(s.size() - 2) == s.peek())) {
             s.pop();
             s.pop();
-
             return 2;
+        } else {
+            return 0;
         }
-
-        return 0;
     }
 }
