2908ec7c8d5eeceae0d9036d935c0aabe2471408
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2026-02-28 15:47:18 +0900

    [level 3] Title: 표 편집, Time: 166.23 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 class Solution {
-
     public String solution(int n, int k, String[] cmd) {
         // ! 기존의 Node 객체로 더블링크드리스트를 구현하면 OOM이 터지니 배열 기반으로 구현
         // 객체 대신 인덱스로 연결하기(배열의 인덱스를 포인터처럼 사용)
@@ -9,7 +8,7 @@ class Solution {
         int[] next = new int[n];
 
         // n만큼 초기화
-        // 행 번호를 Node, prev[i] 또는 next[i]를 ptr.next라고 생각하자
+        // ? prev[i] 또는 next[i]는 행 번호이면서 동시에 포인터 역할을 함
         for (int i = 0; i < n; i++) {
             prev[i] = i - 1; // 0번 이전은 없으니까 -1 표시
             next[i] = i + 1; // 마찬가지로 n번 다음은 없으니까 그대로 n + 1 표시
@@ -33,13 +32,13 @@ class Solution {
                 case 'U':
                     int x = Integer.parseInt(c.substring(2));
                     while (x-- > 0) {
-                        k = prev[k]; // 이전 행으로 포인터를 갱신(≒ ptr = ptr.prev;)
+                        k = prev[k]; // int 타입은 값을 복사하므로, k가 이전 행으로 업데이트(≒ ptr = ptr.prev;)
                     }
                     break;
                 case 'D':
                     int y = Integer.parseInt(c.substring(2));
                     while (y-- > 0) {
-                        k = next[k]; // 다음 행으로 포인터를 갱신(≒ ptr = ptr.next;)
+                        k = next[k]; // 마찬가지로 k가 다음 행으로 업데이트(≒ ptr = ptr.next;)
                     }
                     break;
                 case 'C':
@@ -47,29 +46,31 @@ class Solution {
                     s.push(k);
                     isDeleted[k] = true;
 
-                    // 핵심: 가드 조건과 실행문의 인덱스를 일치시켜야 합니다!
-                    // 1. 내 다음 행(next[k])이 있다면, 그 행의 이전(prev)을 내 이전(prev[k])으로 연결
-                    if (next[k] != -1) {
-                        prev[next[k]] = prev[k];
-                    }
-                    // 2. 내 이전 행(prev[k])이 있다면, 그 행의 다음(next)을 내 다음(next[k])으로 연결
+                    // ? 행끼리 연결하기 위해서는 행이 존재해야 된다, 즉 prev[k] 또는 next[k]가 존재해야 한다는 말
+                    //* Singly Linked List가 익숙하기 때문에 다음 행 연결을 먼저 작성하였음.
+                    // "2번 말고 3번으로 가!"가 실행되기 위해서는 k가 가리키고 있는 행의 이전 행이 존재해야 함
                     if (prev[k] != -1) {
                         next[prev[k]] = next[k];
+                    }
+                    // "2번 말고 1번으로 가!"가 실행되기 위해서는 k가 가리키고 있는 행의 다음 행이 존재해야 함
+                    if (next[k] != -1) {
+                        prev[next[k]] = prev[k];
                     }
 
                     // 현재 위치 이동: 다음 행이 없으면 위로(마지막 행 삭제 시), 있으면 아래로
                     k = (next[k] == -1) ? prev[k] : next[k];
                     break;
                 case 'Z':
-                    // 삭제한 행을 다시 복구(Doubly Linked List의 insert와 유사)
+                    //? 2번 사물함 안의 메모지는 여전히 "내 왼쪽은 1번, 오른쪽은 3번"을 기억하고 있음
+                    // 삭제한 행을 다시 복구하는데, 위의 'C'에서 사용한 k가 z로만 변경된 것에 불과
                     int z = s.pop();
                     isDeleted[z] = false;
-                    // prev[z]와 next[z]는 여전히 배열에 남아있기 때문에 그대로 z에만 연결
+                    // "이제 3번 말고 2번과 연결해"!가 실행되기 위해서는 복구한 2번을 그대로 연결만 하면 됨
                     if (prev[z] != -1) {
-                        next[prev[z]] = z; // prev[z]는 A
+                        next[prev[z]] = z; // z가 복구한 숫자
                     }
                     if (next[z] != -1) {
-                        prev[next[z]] = z; // next[z]는 B
+                        prev[next[z]] = z; // 마찬가지
                     }
                     break;
             }
@@ -87,5 +88,6 @@ class Solution {
 
         return sb.toString();
     };
+
 
 }
