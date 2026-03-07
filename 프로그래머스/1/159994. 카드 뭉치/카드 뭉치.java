91b851eabce2d01e9d55d3d41e5da69a94aa69d3
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2026-03-07 12:48:40 +0900

    [level 1] Title: 카드 뭉치, Time: 0.10 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -2,35 +2,32 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        // 기존에 주어진 카드 뭉치의 단어 순서는 바꿀 수 없습니다.
-        Queue<String> q1 = new ArrayDeque<>();
-        Queue<String> q2 = new ArrayDeque<>();
+        // 먼저 카드들을 순서대로 한 장씩 사용하기 위해 큐로 초기화한다. 단, 인터페이스의 유용성을 위해 Deque를 사용
+        Deque<String> d1 = new ArrayDeque<>();
+        Deque<String> d2 = new ArrayDeque<>();
 
-        // 원하는 카드 뭉치에서 카드를 순서대로 한 장씩 사용합니다.
         for (String c1 : cards1) {
-            q1.offer(c1);
+            d1.offer(c1);
         }
 
         for (String c2 : cards2) {
-            q2.offer(c2);
+            d2.offer(c2);
         }
 
-        // 먼저 goal을 순회
-        // goal의 단어를 하나씩 만들 수 있다면 ""로 변경
-        int answer = goal.length;
-        for (int i = 0; i < goal.length; i++) {
-            if (!q1.isEmpty() && q1.peek().equals(goal[i])) {
-                q1.poll();
-                answer--;
+        // goal을 순회
+        for (String g : goal) {
+            if (!d1.isEmpty() && (d1.peek().equals(g))) {
+                d1.poll();
+                continue;
+            } else if (!d2.isEmpty() && (d2.peek().equals(g))) {
+                d2.poll();
+                continue;
+            } else {
+                return "No";
             }
-            if (!q2.isEmpty() && q2.peek().equals(goal[i])) {
-                q2.poll();
-                answer--;
-            }
-            // 카드를 사용하지 않고 다음 카드로 넘어갈 수 없습니다.
-            
         }
-
-        return answer == 0 ? "Yes" : "No";
+        // goal의 모든 단어를 다 찾았다면, 카드 뭉치에 잔여물이 있더라도 성공!
+        // ! "2 ≤ goal의 길이 ≤ cards1의 길이 + cards2의 길이"
+        return "Yes";
     }
 }
