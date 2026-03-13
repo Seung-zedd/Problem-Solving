7dd383258d2236802ea8d3c2762daf4c53609bc7
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2026-03-13 14:42:03 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.00 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,26 +2,26 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> map = new HashMap<>();
-        // 먼저 참가자 수를 순회하면서 카운팅을 한다
+        // [복습 3회차] 완주하지 못한 선수
+        Map<String, Integer> marathonMap = new HashMap<>();
+
+        // 참가자 먼저 카운팅
         for (String p : participant) {
-            map.put(p, map.getOrDefault(p, 0) + 1);
+            marathonMap.merge(p, 1, Integer::sum);
+        }
+        // 완주자 매칭
+        // ! 람다식은 새로운 변수를 넣어야함
+        for (String c : completion) {
+            marathonMap.computeIfPresent(c, (k, v) -> v == 1 ? null : v - 1);
         }
 
-        // 완주자 수를 순회하면서 참가자 수와 매칭
-        for (String c : completion) {
-            if (map.containsKey(c)) {
-                map.put(c, map.getOrDefault(c, 0) - 1);
-            }
-        }
-        
-        // 다시 참가자 수를 순회하면서 카운팅된 값이 남아있으면 그것이 곧 완주하지 못한 선수
+        // 완주하지 못한 선수 리턴
         for (String p : participant) {
-            if (map.get(p) != 0) {
+            if (marathonMap.get(p) != null) {
                 return p;
             }
         }
-        
+
         return "";
     }
 }
