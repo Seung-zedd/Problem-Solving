4333315e55067fdb1ac6de0e164b709e9ff32526
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2026-03-14 15:19:05 +0900

    [level 1] Title: 카드 뭉치, Time: 0.09 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/README.md
+++ 프로그래머스/1/159994. 카드 뭉치/README.md
@@ -1,10 +1,10 @@
 # [level 1] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994#) 
 
 ### 성능 요약
 
-메모리: 80.7 MB, 시간: 0.12 ms
+메모리: 73.3 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 10일 14:37:07
+2026년 03월 14일 15:18:59
 
 ### 문제 설명
 
--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -2,28 +2,33 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        // 원하는 카드 뭉치에서 카드를 순서대로 한 장씩 사용하기 위해 큐로 초기화
+        // [복습 4회차] 카드 뭉치
         Deque<String> d1 = new ArrayDeque<>();
         Deque<String> d2 = new ArrayDeque<>();
 
-        for (String c1 : cards1) {
-            d1.offer(c1);
+        // 카드를 순서대로 꺼내기 위해 덱으로 삽입
+        for (String c : cards1) {
+            d1.offer(c);
         }
-        for (String c2 : cards2) {
-            d2.offer(c2);
+
+        for (String c : cards2) {
+            d2.offer(c);
         }
-        
-        // goal을 순회
+
+        // goal을 순회하면서 단어 매칭 시작
+        //? 2 ≤ goal의 길이 ≤ cards1의 길이 + cards2의 길이
         for (String g : goal) {
-            if (!d1.isEmpty() && (d1.peek().equals(g))) {
+            if (!d1.isEmpty() && d1.peek().equals(g)) {
                 d1.poll();
                 continue;
-            } else if (!d2.isEmpty() && (d2.peek().equals(g))) {
+            } else if (!d2.isEmpty() && d2.peek().equals(g)) {
                 d2.poll();
                 continue;
             }
+            // d1, d2 모두에 매칭되는 단어가 없으면 No를 리턴
             return "No";
         }
+
         return "Yes";
     }
 }
