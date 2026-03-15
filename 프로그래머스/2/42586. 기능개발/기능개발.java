5d08b761ea04ac5f78c59286d9841253475cf96e
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2026-03-15 18:02:38 +0900

    [level 2] Title: 기능개발, Time: 3.04 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,40 +2,37 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
+        // 순서를 위해 덱을 기반으로 하는 큐 생성
         Deque<Integer> q = new ArrayDeque<>();
 
-        // 하루에 배포 가능한 날짜를 계산
+        // 남은 개발 일짜 계산
         for (int i = 0; i < progresses.length; i++) {
             int day = (100 - progresses[i]) / speeds[i];
-            int remained = (100 - progresses[i]) % speeds[i];
+            int remain = (100 - progresses[i]) % speeds[i];
 
-            // 나누어떨어지지 않았으면 date 1 추가
-            if (remained != 0) {
-                day += 1;
+            // 나누어 떨어지지 않으면 day 1 증가
+            if (remain != 0) {
+                day++;
             }
 
-            // 계산한 날짜를 큐에 담는다
             q.offer(day);
         }
-
-        // 하루에 배포 가능한 기능 개수 카운팅
-        // 테케를 보면 파라미터의 배열 길이와 리턴 배열 길이가 일치하지 않기 때문에 가변 배열인 리스트 사용
-        List<Integer> ans = new ArrayList<>();
+        
+        // 배포 날짜 계산 시작
+        List<Integer> answer = new ArrayList<>();
         while (!q.isEmpty()) {
-            // 큐를 순회할 때마다 기능 계산 시작
             int feature = 0;
             int maxDate = q.poll();
-            feature++; // 큐에서 꺼냈으면 기능 카운팅
-
-            while (!q.isEmpty() && maxDate >= q.peek()) {
+            feature++; // 꺼냈으면 기능 1 추가
+            // 단조 스택의 맹공 스킬을 활용해서 계속해서 잡아먹는다
+            while (!q.isEmpty() && (maxDate >= q.peek())) {
                 q.poll();
-                feature++;
+                feature++; 
             }
-            
-            ans.add(feature);
+
+            answer.add(feature);
         }
 
-        return ans.stream().mapToInt(Integer::intValue).toArray();
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
-
 }
