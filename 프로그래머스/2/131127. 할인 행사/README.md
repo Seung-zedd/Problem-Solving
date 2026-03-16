993d20277f4c7123f3702a8f4305db816b04f02b
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2026-03-16 13:41:09 +0900

    [level 2] Title: 할인 행사, Time: 79.02 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 128 MB, 시간: 82.65 ms
+메모리: 116 MB, 시간: 79.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 13일 14:29:16
+2026년 03월 16일 13:41:03
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,37 +2,34 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
-        // [복습 1회차] 할인 행사
-        int count = 0;
-
-        // 단일 슬라이딩 윈도우 기법으로 문제 돌파
+        // 1. 해시맵 초기화 후 wantMap에 <want, number>를 먼저 담음
         Map<String, Integer> wantMap = new HashMap<>();
-        // 제품, 수량 삽입(최대 길이 10)
         for (int i = 0; i < want.length; i++) {
-            wantMap.put(want[i], number[i]);
+            wantMap.merge(want[i], number[i], Integer::sum);
         }
 
-        // discount 슬라이딩 윈도우 초기 세팅
+        // 2. windowMap에도 우선 초기 10일차의 품목을 담음
         Map<String, Integer> windowMap = new HashMap<>();
         for (int i = 0; i < 10; i++) {
             windowMap.merge(discount[i], 1, Integer::sum);
         }
-        // 처음 1~10일차 비교
+
+        // 회원등록시 정현이가 원하는 제품을 모두 할인 받을 수 있는 회원등록 날짜의 총 일수
+        int count = 0;
         if (wantMap.equals(windowMap)) {
             count++;
         }
 
-        // 슬라이딩 윈도우 시작
+        // 3. 단일 루프 슬라이딩 윈도우 시작
         for (int i = 10; i < discount.length; i++) {
-            String outItem = discount[i - 10];
-
             // A) 나가는 품목
-            //? 수량이 1이면 연산 결과가 null이 되는 순간 해당 키를 즉시 remove, 그렇지 않으면 수량 1을 감소
-            windowMap.computeIfPresent(outItem, (k, v) -> v == 1 ? null : v - 1);
+            String outItem = discount[i - 10];
+            windowMap.computeIfPresent(outItem, (k, v) -> v == 1 ? null : v - 1); // 1이면 아예 null을 리턴해서 품목을 없앰
 
-            // B) 들어가는 품목
+            // B) 들어오는 품목
             String inItem = discount[i];
             windowMap.merge(inItem, 1, Integer::sum);
+
             // C) 검증
             if (wantMap.equals(windowMap)) {
                 count++;
