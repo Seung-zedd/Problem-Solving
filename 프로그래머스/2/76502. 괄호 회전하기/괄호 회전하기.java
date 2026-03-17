3332443c262f2b6feb1e70285d29ae75b7864e94
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2026-03-17 12:00:15 +0900

    [level 2] Title: 괄호 회전하기, Time: 158.83 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,40 +1,54 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int solution(String s) {
+        // 0 ≤ x < (s의 길이)로 회전하기 위해 그냥 s를 for문으로 순회
         int answer = 0;
-        // 1. s를 0 ~ s.len - 1 만큼 순회하기 위해 for문 사용
-        for (int i = 0; i < s.length(); i++) {
-            String str = leftLotate(i, s);
-            answer += isValid(str);
-        }
 
+        for (int i = 0; i < s.length(); i++) {
+            String result = leftRotate(i, s);
+            answer += validParenthesis(result);
+        }
         return answer;
     }
 
-    private static String leftLotate(int x, String s) {
-        // x만큼 왼쪽으로 회전: x부터 끝까지 + 처음부터 x전까지
-        return s.substring(x) + s.substring(0, x);
+    private static String leftRotate(int x, String s) {
+        // 원형 큐 초기화
+        Deque<Character> q = new ArrayDeque<>();
+        // 원형 큐에 문자열 삽입
+        for (int i = 0; i < s.length(); i++) {
+            q.offer(s.charAt(i));
+        }
+
+        // 요세푸스 순열 응용
+        for (int i = 0; i < x; i++) {
+            q.offer(q.poll());
+        }
+
+        return q.stream().map(String::valueOf).collect(Collectors.joining());
     }
 
-    private static int isValid(String s) {
-        Stack<Character> st = new Stack<>();
-
+    private static int validParenthesis(String s) {
+        // 스택 초기화
+        Deque<Character> st = new ArrayDeque<>();
+        // 스택에 문자열 삽입
         for (int i = 0; i < s.length(); i++) {
             Character c = s.charAt(i);
-            // 괄호 짝 매칭: !st.isEmpty()가 모든 || 조건을 보호하도록 괄호로 묶음
-            //! 연산자 순서가 애매하면 괄호로 안전하게 묶자!
-            if (!st.isEmpty() && ((st.peek() == '(' && c == ')') ||
-                    (st.peek() == '{' && c == '}') ||
-                    (st.peek() == '[' && c == ']'))) {
-                st.pop();
+            // 짝이 맞으면 pop()
+            if (!st.isEmpty() && ((st.peekLast() == '(' && c == ')') || st.peekLast() == '{' && c == '}' || st
+                    .peekLast() == '[' && c == ']')) {
+                st.pollLast();
             } else if (c == ')' || c == '}' || c == ']') {
-                // 짝이 맞지 않는 닫힌 괄호가 오거나 스택이 비어있는데 닫힌 괄호가 온 경우
                 return 0;
-            } else {
-                st.push(c);
+            }
+            // 열린 괄호는 무조건 push
+            else {
+                st.offer(c);
             }
         }
+
         return st.isEmpty() ? 1 : 0;
+
     }
 }
