bc8daccac2d066ea3eadb95c9c6ce1d026672a5e
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2026-03-30 18:00:25 +0900

    [level 1] Title: 신고 결과 받기, Time: 217.29 ms, Memory: 159 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/README.md
+++ 프로그래머스/1/92334. 신고 결과 받기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 신고 결과 받기 - 92334 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334#) 
 
 ### 성능 요약
 
-메모리: 154 MB, 시간: 179.63 ms
+메모리: 159 MB, 시간: 217.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 21일 14:27:09
+2026년 03월 30일 18:00:18
 
 ### 문제 설명
 
--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,22 +2,21 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = new int[id_list.length];
-
-        // 1. report를 순회하면서 신고
-        // <String, Set<String>>
+        int[] ans = new int[id_list.length];
+        // 1.2. 한 유저를 여러 번 신고할 수도 있지만, **동일한 유저에 대한 신고 횟수는 1회로 처리**됩니다.
         Map<String, Set<String>> reportMap = new HashMap<>();
+
+        // 1. 신고자 -> 신고 받은 해시맵을 먼저 집계
+        // report의 원소는 "이용자id 신고한id"형태의 문자열입니다.
         for (String r : report) {
             String[] msg = r.split(" ");
             String userId = msg[0];
-            String reportedId = msg[1];
-
-            reportMap.computeIfAbsent(userId, u -> new HashSet<>()).add(reportedId);
+            String reportId = msg[1];
+            reportMap.computeIfAbsent(userId, key -> new HashSet<>()).add(reportId);
         }
 
         // 2. k번 이상 신고된 유저를 계산
-        // reportMap의 Value를 순회하면서 신고당한 유저 카운팅
-        // 시간복잡도: O(N + K), 합연산인 이유는 set의 총합인 K를 분할 및 소집하기 때문
+        // O(N + K): 분할 및 소집
         Map<String, Integer> countMap = new HashMap<>();
         for (Set<String> reportSet : reportMap.values()) {
             for (String r : reportSet) {
@@ -25,26 +24,21 @@ class Solution {
             }
         }
 
-        // 3. 정지 메일 발송
-        // 주체: userId
+        // 3. k번 이상 신고된 유저가 있으면 신고자한테 정지 메일 발송
         for (int i = 0; i < id_list.length; i++) {
             String userId = id_list[i];
-
-            // userId한테 메일을 발송하기 위함
+            // 내가 누구를 신고했지?
             Set<String> reportSet = reportMap.get(userId);
-
-            // 누가 k번 이상을 받았을까요?
-            //! "neo" 없음 없음와 같이 신고한 유저가 없을 수도 있음!
+            // 신고를 안 한 사람이 있을 수도 있으므로
             if (reportSet != null) {
                 for (String r : reportSet) {
-                    // k번 이상 신고 당하면
                     if (countMap.get(r) >= k) {
-                        answer[i]++; // userId가 있는 인덱스에 메일 발송
+                        ans[i]++;
                     }
                 }
             }
         }
-        
-        return answer;
+
+        return ans;
     }
 }
