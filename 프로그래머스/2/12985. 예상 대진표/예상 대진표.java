6a24615038e196482ce246fc37a5e37c0840672d
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2026-04-04 20:04:43 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,14 +2,15 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int round = 0; //
-
-        while (a != b) {
-            a = (a + 1) / 2;
-            b = (b + 1) / 2;
-            round++;
+        //? solution()의 정의: a와 b는 도대체 몇 라운드에 만날 것인가?
+        // base condition
+        // a와 b의 다음 라운드 번호가 같을 때(즉, 이번에 맞붙을 때), 더 이상 남은 라운드는 없고 이번 1경기로 끝난다.
+        if ((a + 1) / 2 == (b + 1) / 2) {
+            return 1;
         }
 
-        return round;
+        // ?의 정체: ?는 현재 내 상태를 의미함, 즉 전체적인 F(n)은 모르겠고 지금 내가 여기서 1경기를 치렀으니 전체 라운드 수에 1만큼 기여했다. 
+        // F(next)의 정체: 다음 라운드 번호로 바뀐 애들 (a+1)/2, (b+1)/2이 만나는 건 다음 함수 네가 알아서 계산해서 가져와!
+        return 1 + solution(n, (a + 1) / 2, (b + 1) / 2); 
     }
 }
