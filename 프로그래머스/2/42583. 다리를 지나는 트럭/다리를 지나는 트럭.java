88849a40ace712716daadcd7614a6bb1fa85b14d
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2026-05-07 18:19:04 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 10.51 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,46 +2,38 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        // 정해진 순으로 건너야 하기 때문에 대기 큐에 트럭 배열 삽입 후 초기화
-        // 1. 다리에 오르기 전
-        Queue<Truck> waitingQ = new LinkedList<>();
+        // 트럭 대기큐 삽입
+        Deque<int[]> waitingQ = new ArrayDeque<>();
         for (int t : truck_weights) {
-            waitingQ.offer(new Truck(t, 0));
+            waitingQ.offer(new int[] {t, 0});
         }
 
         // 시뮬레이션 시작
-        Queue<Truck> bridgeQ = new LinkedList<>();
         int time = 0;
-        int totalWeight = 0;
-
+        int totalTruckWeight = 0; // 다리에 올라가는 트럭의 총 무게
+        Deque<int[]> bridgeQ = new ArrayDeque<>();
+        // 전체가 비어있으면 종료 <=> 대기큐가 비어있지 않거나 다리큐가 비어있지 않으면 진행
         while (!waitingQ.isEmpty() || !bridgeQ.isEmpty()) {
             time++;
 
-            // 선 제거, 후 삽입
-            if (!bridgeQ.isEmpty() && time - bridgeQ.peek().entryTime == bridge_length) {
-                Truck exitTruck = bridgeQ.poll();
-                totalWeight -= exitTruck.tWeight;
+            // 다리에서 내려감(버퍼가 꽉 차있으면 먼저 응용층으로 올려보내는게 맞음, 그런데 전송 알고리즘은 하나만 동작하기 때문에 나가는 것을 먼저 작성한다)
+            if (!bridgeQ.isEmpty() && (time - bridgeQ.peek()[1] == bridge_length)) {
+                int outTruck = bridgeQ.poll()[0];
+                totalTruckWeight -= outTruck;
             }
 
-            if (!waitingQ.isEmpty() && waitingQ.peek().tWeight + totalWeight <= weight && bridgeQ.size() < bridge_length) {
-                Truck curTruck = waitingQ.poll();
-                curTruck.entryTime = time;
-
-                bridgeQ.offer(curTruck);
-                totalWeight += curTruck.tWeight;
+            // 다리로 올라감(다리에는 트럭이 최대 bridge_length대 올라갈 수 있으며, 다리는 weight 이하까지의 무게를 견딜 수 있습니다.)
+            //! 항상 peek()을 사용하면 자동반사적으로 null-safety 프로그래밍을 하자
+            if (!waitingQ.isEmpty() && (totalTruckWeight + waitingQ.peek()[0] <= weight && bridgeQ.size() < bridge_length)) {
+                int[] truck = waitingQ.poll();
+                int inTruck = truck[0];
+                int entryTime = truck[1];
+                entryTime = time;
+                bridgeQ.offer(new int[] {inTruck, entryTime});
+                totalTruckWeight += inTruck;
             }
         }
 
         return time;
-    }
-
-    private static class Truck {
-        int tWeight;
-        int entryTime;
-
-        public Truck(int tWeight, int entryTime) {
-            this.tWeight = tWeight;
-            this.entryTime = entryTime;
-        }
     }
 }
